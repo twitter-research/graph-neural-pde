fc4b4fefd94f9a16e28586d2709f89f47543cf7e
Ben Chamberlain <bchamberlain@twitter.com>
2021-01-21 19:53:15 +0000

    removed reg

--- src/block_constant.py
+++ src/block_constant.py
@@ -33,10 +33,9 @@ class ConstantODEblock(ODEblock):
     
     reg_states = tuple( torch.zeros(x.size(0)).to(x) for i in range(self.nreg) )
 
-    # func = self.reg_odefunc if self.training else self.odefunc
-    func = self.odefunc
-    # state = (x,) + reg_states if self.training else x
-    state = x
+    func = self.reg_odefunc if self.training and self.nreg > 0 else self.odefunc
+    state = (x,) + reg_states if self.training and self.nreg > 0 else x
+
     if self.opt["adjoint"]:
       state_dt = integrator(
         func, state, t,
@@ -53,12 +52,10 @@ class ConstantODEblock(ODEblock):
         atol=self.atol,
         rtol=self.rtol)
 
-    if self.training:
-      # z = state_dt[0][1]
-      # reg_states = tuple( st[1] for st in state_dt[1:] )
-      # return z, reg_states
-      z = state_dt[1]
-      return z, None
+    if self.training and self.nreg > 0:
+      z = state_dt[0][1]
+      reg_states = tuple( st[1] for st in state_dt[1:] )
+      return z, reg_states
     else: 
       z = state_dt[1]
       return z
