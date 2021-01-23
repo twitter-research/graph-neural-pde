39007cac76f3bcb3eef79d3553372bf186074fdf
Ben Chamberlain <bchamberlain@twitter.com>
2021-01-23 13:46:59 +0000

    fixed step size search space

--- src/ray_tune.py
+++ src/ray_tune.py
@@ -498,9 +498,9 @@ def set_arxiv_search_space(opt):
   opt["input_dropout"] = 0
   # opt["dropout"] = tune.uniform(0, 0.2)
   opt["dropout"] = 0
-  opt['step_size'] = tune.uniform(0.5, 2)
-  opt['adjoint_step_size'] = tune.uniform(0.5, 2)
-  opt["time"] = tune.uniform(2, 10.0)
+  opt['step_size'] = tune.choice([0,25, 0.5, 1, 2])
+  opt['adjoint_step_size'] = tune.choice([0,25, 0.5, 1, 2])
+  opt["time"] = tune.uniform([1,2,3,4,5,6,7,8,9,10])
   # opt["optimizer"] = tune.choice(["adam", "adamax", "rmsprop"])
   opt['optimizer'] = 'adam'
   if opt["block"] in {'attention', 'mixed'} or opt['function'] in {'GAT', 'transformer', 'dorsey'}:
