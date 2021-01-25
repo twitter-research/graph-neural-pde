082a8b2c9ad098f2b7a67b88e0431f091b928b20
jr1419 <jr1419@ic.ac.uk>
2021-01-25 11:37:24 +0000

    new image/ani generation function

---
+++ src/block_transformer_hard_attention.py
@@ -0,0 +1,85 @@
+import torch
+from function_transformer_attention import SpGraphTransAttentionLayer
+from base_classes import ODEblock
+from utils import get_rw_adj
+
+
+class HardAttODEblock(ODEblock):
+  def __init__(self, odefunc, regularization_fns, opt, data, device, t=torch.tensor([0, 1]), gamma=0.5):
+    super(HardAttODEblock, self).__init__(odefunc, regularization_fns, opt, data, device, t)
+
+    self.odefunc = odefunc(self.aug_dim * opt['hidden_dim'], self.aug_dim * opt['hidden_dim'], opt, data, device)
+    # self.odefunc.edge_index, self.odefunc.edge_weight = data.edge_index, edge_weight=data.edge_attr
+    edge_index, edge_weight = get_rw_adj(data.edge_index, edge_weight=data.edge_attr, norm_dim=1,
+                                         fill_value=opt['self_loop_weight'],
+                                         num_nodes=data.num_nodes,
+                                         dtype=data.x.dtype)
+    self.data_edge_index = edge_index
+    self.odefunc.edge_index = edge_index.to(device)  # this will be changed by attention scores
+    self.odefunc.edge_weight = edge_weight.to(device)
+    self.reg_odefunc.odefunc.edge_index, self.reg_odefunc.odefunc.edge_weight = self.odefunc.edge_index, self.odefunc.edge_weight
+
+    if opt['adjoint']:
+      from torchdiffeq import odeint_adjoint as odeint
+    else:
+      from torchdiffeq import odeint
+    self.train_integrator = odeint
+    self.test_integrator = odeint
+    self.set_tol()
+    # parameter trading off between attention and the Laplacian
+    self.multihead_att_layer = SpGraphTransAttentionLayer(opt['hidden_dim'], opt['hidden_dim'], opt,
+                                                          device, edge_weights=self.odefunc.edge_weight).to(device)
+
+  def get_attention_weights(self, x):
+    attention, values = self.multihead_att_layer(x, self.data_edge_index)
+    return attention
+
+  def forward(self, x):
+    t = self.t.type_as(x)
+    attention_weights = self.get_attention_weights(x)
+    # create attention mask
+    with torch.no_grad():
+      mean_att = attention_weights.mean(dim=1, keepdim=False)
+      threshold = torch.quantile(mean_att, 0.5)
+      mask = mean_att > threshold
+      self.odefunc.edge_index = self.data_edge_index[:, mask.T]
+      print('retaining {} of {} edges'.format(self.odefunc.edge_index.shape[1], self.data_edge_index.shape[1]))
+      self.odefunc.attention_weights = attention_weights[mask]
+    self.reg_odefunc.odefunc.attention_weights = self.odefunc.attention_weights
+    integrator = self.train_integrator if self.training else self.test_integrator
+
+    reg_states = tuple(torch.zeros(x.size(0)).to(x) for i in range(self.nreg))
+
+    func = self.reg_odefunc if self.training and self.nreg > 0 else self.odefunc
+    state = (x,) + reg_states if self.training and self.nreg > 0 else x
+
+    if self.opt["adjoint"]:
+      state_dt = integrator(
+        func, state, t,
+        method=self.opt['method'],
+        options={'step_size': self.opt['step_size']},
+        adjoint_method=self.opt['adjoint_method'],
+        adjoint_options={'step_size': self.opt['adjoint_step_size']},
+        atol=self.atol,
+        rtol=self.rtol,
+        adjoint_atol=self.atol_adjoint,
+        adjoint_rtol=self.rtol_adjoint)
+    else:
+      state_dt = integrator(
+        func, state, t,
+        method=self.opt['method'],
+        options={'step_size': self.opt['step_size']},
+        atol=self.atol,
+        rtol=self.rtol)
+
+    if self.training and self.nreg > 0:
+      z = state_dt[0][1]
+      reg_states = tuple(st[1] for st in state_dt[1:])
+      return z, reg_states
+    else:
+      z = state_dt[1]
+      return z
+
+  def __repr__(self):
+    return self.__class__.__name__ + '( Time Interval ' + str(self.t[0].item()) + ' -> ' + str(self.t[1].item()) \
+           + ")"
