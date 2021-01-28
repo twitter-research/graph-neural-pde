7f7905281130f83c2f65cb1111078813a2db04c6
Ben Chamberlain <bchamberlain@twitter.com>
2021-01-28 11:50:24 +0000

    cmd args

--- src/ray_tune.py
+++ src/ray_tune.py
@@ -678,6 +678,8 @@ if __name__ == "__main__":
   parser.add_argument('--ppr_alpha', type=float, default=0.05, help="teleport probability")
   parser.add_argument('--heat_time', type=float, default=3., help="time to run gdc heat kernal diffusion for")
   parser.add_argument("--not_lcc", action="store_false", help="don't use the largest connected component")
+  parser.add_argument('--use_flux', dest='use_flux', action='store_true',
+                      help='incorporate the feature grad in attention based edge dropout')
   parser.add_argument("--exact", action="store_true",
                       help="for small datasets can do exact diffusion. If dataset is too big for matrix inversion then you can't")
   parser.add_argument('--att_samp_pct', type=float, default=1, help="float in [0,1). The percentage of edges to retain based on attention scores")
