c71dbe2448bfb333ae3bef108aaa7b015346fd48
Ben Chamberlain <bchamberlain@twitter.com>
2021-01-28 19:30:58 +0000

    made label use compatible with datasets beyond just arxiv.

--- src/run_GNN.py
+++ src/run_GNN.py
@@ -7,6 +7,7 @@ import time
 from data import get_dataset
 from ogb.nodeproppred import Evaluator
 
+
 def get_cora_opt(opt):
   opt['dataset'] = 'Cora'
   opt['data'] = 'Planetoid'
@@ -66,23 +67,38 @@ def get_optimizer(name, parameters, lr, weight_decay=0):
   else:
     raise Exception("Unsupported optimizer: {}".format(name))
 
+
 def add_labels(feat, labels, idx, num_classes, device):
   onehot = torch.zeros([feat.shape[0], num_classes]).to(device)
-  # onehot[idx, labels[idx]] = 1
-  onehot[idx, labels[idx, 0]] = 1
+  if idx.dtype == torch.bool:
+    idx = torch.where(idx)[0]  # convert mask to linear index
+  onehot[idx, labels.squeeze()[idx]] = 1
+  # onehot[idx, labels[idx, 0]] = 1
   return torch.cat([feat, onehot], dim=-1)
+
+
+def get_label_masks(data, mask_rate=0.5):
+  """
+  when using labels as features need to split training nodes into training and prediction
+  """
+  if data.train_mask.dtype == torch.bool:
+    idx = torch.where(data.train_mask)[0]
+  else:
+    idx = data.train_mask
+  mask = torch.rand(idx.shape) < mask_rate
+  train_label_idx = idx[mask]
+  train_pred_idx = idx[~mask]
+  return train_label_idx, train_pred_idx
+
 
 def train(model, optimizer, data):
   model.train()
   optimizer.zero_grad()
   feat = data.x
   if model.opt['use_labels']:
-    mask_rate = 0.5
-    mask = torch.rand(data.train_mask.shape) < mask_rate
+    train_label_idx, train_pred_idx = get_label_masks(data)
 
-    train_labels_idx = data.train_mask[mask]
-    train_pred_idx = data.train_mask[~mask]
-    feat = add_labels(feat, data.y, train_labels_idx, model.num_classes, model.device)
+    feat = add_labels(feat, data.y, train_label_idx, model.num_classes, model.device)
   else:
     # todo in the DGL code from OGB, they seem to apply an additional mask to the training data - currently commented
     # mask_rate = 0.5
@@ -138,35 +154,37 @@ def print_model_params(model):
       print(name)
       print(param.data.shape)
 
+
 @torch.no_grad()
 def test_OGB(model, data, opt):
-    if opt['dataset'] == 'ogbn-arxiv':
-      name = 'ogbn-arxiv'
+  if opt['dataset'] == 'ogbn-arxiv':
+    name = 'ogbn-arxiv'
 
-    feat = data.x
-    if model.opt['use_labels']:
-        feat = add_labels(feat, data.y, data.train_mask, model.num_classes, model.device)
+  feat = data.x
+  if model.opt['use_labels']:
+    feat = add_labels(feat, data.y, data.train_mask, model.num_classes, model.device)
 
-    evaluator = Evaluator(name=name)
-    model.eval()
+  evaluator = Evaluator(name=name)
+  model.eval()
 
-    out = model(feat).log_softmax(dim=-1)
-    y_pred = out.argmax(dim=-1, keepdim=True)
+  out = model(feat).log_softmax(dim=-1)
+  y_pred = out.argmax(dim=-1, keepdim=True)
 
-    train_acc = evaluator.eval({
-        'y_true': data.y[data.train_mask],
-        'y_pred': y_pred[data.train_mask],
-    })['acc']
-    valid_acc = evaluator.eval({
-        'y_true': data.y[data.val_mask],
-        'y_pred': y_pred[data.val_mask],
-    })['acc']
-    test_acc = evaluator.eval({
-        'y_true': data.y[data.test_mask],
-        'y_pred': y_pred[data.test_mask],
-    })['acc']
+  train_acc = evaluator.eval({
+    'y_true': data.y[data.train_mask],
+    'y_pred': y_pred[data.train_mask],
+  })['acc']
+  valid_acc = evaluator.eval({
+    'y_true': data.y[data.val_mask],
+    'y_pred': y_pred[data.val_mask],
+  })['acc']
+  test_acc = evaluator.eval({
+    'y_true': data.y[data.test_mask],
+    'y_pred': y_pred[data.test_mask],
+  })['acc']
 
-    return train_acc, valid_acc, test_acc
+  return train_acc, valid_acc, test_acc
+
 
 def main(opt):
   try:
@@ -224,23 +242,28 @@ if __name__ == '__main__':
   parser.add_argument('--epoch', type=int, default=10, help='Number of training epochs per iteration.')
   parser.add_argument('--alpha', type=float, default=1.0, help='Factor in front matrix A.')
   parser.add_argument('--alpha_dim', type=str, default='sc', help='choose either scalar (sc) or vector (vc) alpha')
-  parser.add_argument('--no_alpha_sigmoid', dest='no_alpha_sigmoid', action='store_true', help='apply sigmoid before multiplying by alpha')
+  parser.add_argument('--no_alpha_sigmoid', dest='no_alpha_sigmoid', action='store_true',
+                      help='apply sigmoid before multiplying by alpha')
   parser.add_argument('--beta_dim', type=str, default='sc', help='choose either scalar (sc) or vector (vc) beta')
   parser.add_argument('--block', type=str, default='constant', help='constant, mixed, attention, hard_attention, SDE')
   parser.add_argument('--function', type=str, default='laplacian', help='laplacian, transformer, dorsey, GAT, SDE')
   # ODE args
   parser.add_argument('--time', type=float, default=1.0, help='End time of ODE integrator.')
-  parser.add_argument('--augment', action='store_true', help='double the length of the feature vector by appending zeros to stabilist ODE learning')
+  parser.add_argument('--augment', action='store_true',
+                      help='double the length of the feature vector by appending zeros to stabilist ODE learning')
   parser.add_argument('--method', type=str, default='dopri5',
                       help="set the numerical solver: dopri5, euler, rk4, midpoint")
-  parser.add_argument('--step_size', type=float, default=1, help='fixed step size when using fixed step solvers e.g. rk4')
+  parser.add_argument('--step_size', type=float, default=1,
+                      help='fixed step size when using fixed step solvers e.g. rk4')
   parser.add_argument('--max_iters', type=float, default=100, help='maximum number of integration steps')
   parser.add_argument(
     "--adjoint_method", type=str, default="adaptive_heun",
     help="set the numerical solver for the backward pass: dopri5, euler, rk4, midpoint"
   )
-  parser.add_argument('--adjoint', dest='adjoint', action='store_true', help='use the adjoint ODE method to reduce memory footprint')
-  parser.add_argument('--adjoint_step_size', type=float, default=1, help='fixed step size when using fixed step adjoint solvers e.g. rk4')
+  parser.add_argument('--adjoint', dest='adjoint', action='store_true',
+                      help='use the adjoint ODE method to reduce memory footprint')
+  parser.add_argument('--adjoint_step_size', type=float, default=1,
+                      help='fixed step size when using fixed step adjoint solvers e.g. rk4')
   parser.add_argument('--tol_scale', type=float, default=1., help='multiplier for atol and rtol')
   parser.add_argument("--tol_scale_adjoint", type=float, default=1.0,
                       help="multiplier for adjoint_atol and adjoint_rtol")
@@ -261,8 +284,10 @@ if __name__ == '__main__':
                       help='the size to project x to before calculating att scores')
   parser.add_argument('--mix_features', dest='mix_features', action='store_true',
                       help='apply a feature transformation xW to the ODE')
-  parser.add_argument("--max_nfe", type=int, default=1000, help="Maximum number of function evaluations in an epoch. Stiff ODEs will hang if not set.")
-  parser.add_argument('--reweight_attention', dest='reweight_attention', action='store_true', help="multiply attention scores by edge weights before softmax")
+  parser.add_argument("--max_nfe", type=int, default=1000,
+                      help="Maximum number of function evaluations in an epoch. Stiff ODEs will hang if not set.")
+  parser.add_argument('--reweight_attention', dest='reweight_attention', action='store_true',
+                      help="multiply attention scores by edge weights before softmax")
   # regularisation args
   parser.add_argument('--jacobian_norm2', type=float, default=None, help="int_t ||df/dx||_F^2")
   parser.add_argument('--total_deriv', type=float, default=None, help="int_t ||df/dt||^2")
@@ -275,17 +300,18 @@ if __name__ == '__main__':
   parser.add_argument('--gdc_method', type=str, default='ppr', help="ppr, heat, coeff")
   parser.add_argument('--gdc_sparsification', type=str, default='topk', help="threshold, topk")
   parser.add_argument('--gdc_k', type=int, default=64, help="number of neighbours to sparsify to when using topk")
-  parser.add_argument('--gdc_threshold', type=float, default=0.0001, help="obove this edge weight, keep edges when using threshold")
+  parser.add_argument('--gdc_threshold', type=float, default=0.0001,
+                      help="obove this edge weight, keep edges when using threshold")
   parser.add_argument('--gdc_avg_degree', type=int, default=64,
                       help="if gdc_threshold is not given can be calculated by specifying avg degree")
   parser.add_argument('--ppr_alpha', type=float, default=0.05, help="teleport probability")
   parser.add_argument('--heat_time', type=float, default=3., help="time to run gdc heat kernal diffusion for")
-  parser.add_argument('--att_samp_pct', type=float, default=1, help="float in [0,1). The percentage of edges to retain based on attention scores")
+  parser.add_argument('--att_samp_pct', type=float, default=1,
+                      help="float in [0,1). The percentage of edges to retain based on attention scores")
   parser.add_argument('--use_flux', dest='use_flux', action='store_true',
                       help='incorporate the feature grad in attention based edge dropout')
   parser.add_argument("--exact", action="store_true",
                       help="for small datasets can do exact diffusion. If dataset is too big for matrix inversion then you can't")
-
 
   args = parser.parse_args()
 
