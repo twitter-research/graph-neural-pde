40830fdadc18c276cbf217601e798a3c8a228979
jr1419 <jr1419@ic.ac.uk>
2021-02-02 11:40:38 +0000

    Setup forMNIST run with -1,1 rescale before sigmoid. With batched/train size fixed

--- src/image_opt_pixel.py
+++ src/image_opt_pixel.py
@@ -83,8 +83,8 @@ def get_image_opt(opt):
   opt['epoch'] = 128 #32 #2 #2 #3 #1
   opt['batched'] = True
   if opt['testing_code']:
-    opt['batch_size'] = 1 #64 #64  # doing batch size for mnist
-    opt['train_size'] = 1 #512 #1024 #512 #0 #128 #10240 #512 #10240
+    opt['batch_size'] = 64 #64  # doing batch size for mnist
+    opt['train_size'] = 512 #1024 #512 #0 #128 #10240 #512 #10240
     opt['test_size'] =  128 #0  #512#64#128
 
   assert (opt['train_size']) % opt['batch_size'] == 0, "train_size needs to be multiple of batch_size"
