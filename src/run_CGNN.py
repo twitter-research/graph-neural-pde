0990fb6cc4e641784cf9dd97177cb39c2ca58431
Ben Chamberlain <bchamberlain@twitter.com>
2021-02-02 00:11:28 +0000

    bugfix

--- src/run_CGNN.py
+++ src/run_CGNN.py
@@ -2,11 +2,13 @@
 Running CGNN model from ICML 20
 """
 from ray_tune import main, train_ray_rand
+from run_best_ray import run_best_params
 
 for dataset in ['Photo', 'Pubmed', 'Computers', 'CoauthorCS', 'ogbn-arxiv']:
   print('running dataset {}'.format(dataset))
   opt = dict(dataset=dataset)
   opt['name'] = 'CGNN_{}'.format(dataset)
+  opt['folder'] = opt['name']
   # opt['not_lcc'] = False
   if dataset == 'ogbn-arxiv':
     opt['not_lcc'] = False
@@ -29,7 +31,6 @@ for dataset in ['Photo', 'Pubmed', 'Computers', 'CoauthorCS', 'ogbn-arxiv']:
   opt['baseline'] = True
   opt['metric'] = 'accuracy'
   opt['regularise'] = False
-  opt["num_splits"] = 1
   opt['block'] = 'constant'
   opt['function'] = 'laplacian'
   opt['adjoint'] = True
