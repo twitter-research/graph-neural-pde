e9a167456ac7753deb11cfa3f65703fa2dbcc086
Ben Chamberlain <bchamberlain@twitter.com>
2021-02-25 19:04:51 +0000

    arxiv params

--- src/ray_tune.py
+++ src/ray_tune.py
@@ -493,9 +493,9 @@ def set_arxiv_search_space(opt):
   opt["dropout"] = tune.uniform(0, 0.2)
   # opt["dropout"] = 0
   # opt['step_size'] = tune.choice([0.5, 1])
-  opt['step_size'] = 0.1
+  opt['step_size'] = 0.2
   # opt['adjoint_step_size'] = tune.choice([0.5, 1])
-  opt['adjoint_step_size'] = 0.1
+  opt['adjoint_step_size'] = 0.2
   # opt["time"] = tune.choice([1,2,3,4,5,6,7,8,9,10])
   opt['time'] = tune.uniform(2, 8)
   # opt['time'] = 5
