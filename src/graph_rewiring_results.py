20fb3f6e728508a0c2f6de36cca348dcc4483b0b
James Rowbottom <jrowbottom@twitter.com>
2021-04-22 11:44:24 +0100

    DIGL PPR rewiring plot, GAT implementation to think about anisotropic rewiring

--- src/graph_rewiring_results.py
+++ src/graph_rewiring_results.py
@@ -1,60 +1,164 @@
+import argparse
+import numpy as np
+import torch
+from torch_geometric.transforms import GDC
 import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib
+from data import get_dataset, rewire
+from torch_geometric.utils import add_self_loops, is_undirected, to_dense_adj, remove_self_loops, contains_self_loops, homophily_ratio
+from DIGL_data import PPRDataset
+from graph_rewiring import apply_gdc
 
 
-df_rewiring_node = pd.read_csv('../results/GPU_rewiring_node_100seeds.csv',header=[0,1,2])
-pd.options.display.max_rows = 50 #None
-df_rewiring_node
+def main(opt):
+    opt['self_loop_weight'] = None
+    dataset = get_dataset(opt, '../data', use_lcc=True)
+    n = dataset.data.num_nodes
+    edge_index0 = dataset.data.edge_index.detach().clone()
+    print(f"edge_index0 contains_self_loops: {contains_self_loops(edge_index0)}")
+    opt['exact'] = True
+    opt['gdc_sparsification'] = 'topk'  # 'threshold'
+    opt['gdc_threshold'] = 0.01
+    opt['decay'] = 0.05931537406301254  # 0.09604826107599472
+    opt['use_lcc'] = True
+    opt['gdc_k'] = 4
 
-ks = [1 ,2 ,4 ,8 ,16 ,32 ,64 ,128 ,256]
-fig =plt.figure(figsize=(16 ,16))
-for rc, i in enumerate(ks):
-    print(f"target k {i}")
-    k = df_rewiring_node['Unnamed: 2_level_0']['Unnamed: 2_level_1']['k']
-    idx = k.index[k == i].tolist()
-    print(idx)
-    orig = df_rewiring_node['Unnamed: 3_level_0']['Unnamed: 3_level_1']['orig_edges'].iloc[idx]
-    count = df_rewiring_node['final_edges']['count']['Unnamed: 5_level_2'].iloc[idx]
-    # means
-    added = df_rewiring_node['added']['mean']['Unnamed: 4_level_2'].iloc[idx]
-    retained = df_rewiring_node['orig_retained']['mean']['Unnamed: 8_level_2'].iloc[idx]
-    removed = df_rewiring_node['orig_removed']['mean']['Unnamed: 7_level_2'].iloc[idx]
+    # dataset = get_dataset(opt, '../data', use_lcc=True)
+    # data = dataset.data
+    # data = apply_gdc(data, opt, type="combined")
+    # dataset.data = data
+    fig=plt.figure(figsize=(16,16))
+    ks = [1, 2, 4, 8, 16, 32, 64, 128, 256]
+    for rc, k in enumerate(ks):
+        PPRdataset = PPRDataset(
+            name=opt['dataset'],
+            use_lcc=opt['use_lcc'],
+            alpha=opt['ppr_alpha'],
+            k=k,
+            eps=None)#opt['gdc_threshold']
+        # )
 
-    # get indices matching buckets
-    bin_list = []
-    bin_labels = []
-    for j, l in enumerate(ks[:len(ks ) -1]):
-        #         print(j,l,ks[j+1])
-        idx_list = orig.index[(orig >= l) & (orig < ks[ j +1])].tolist()
-        bin_list.append(idx_list)
-        bin_labels.append(f"{l}:<{ks[ j +1]}")
-    if orig.index[orig >= ks[-1]].tolist() != []:
-        bin_list.append(orig.index[orig >= ks[-1]].tolist())
-        bin_labels.append(f">{ks[-1]}")
+        # Loop over target degrees
+        edge_index_PPR = PPRdataset.data.edge_index[0,:]
+        nodes, degrees = torch.unique(edge_index_PPR, return_counts=True)
+        unique_degrees, count_degrees = torch.unique(degrees, return_counts=True)
 
-    print(bin_list)
-    print(bin_labels)
+        # Plot
 
-    # find weighted mean of the 3 parts for each bucket
-    wav_added = []
-    wav_retained = []
-    wav_removed = []
-    for idxs in bin_list:
-        print(idxs)
-        print(f"count[idxs]::: {count[idxs]}")
-        print(f"add {(added[idxs] * count[idxs]).sum( ) /count[idxs].sum()}")
-        print(f"ret {(retained[idxs] * count[idxs]).sum( ) /count[idxs].sum()}")
-        print(f"rem {(removed[idxs] * count[idxs]).sum( ) /count[idxs].sum()}")
-        # wav_added.append((added[idxs] * count[idxs]).sum( ) /count[idxs].sum())
-        # wav_retained.append((retained[idxs] * count[idxs]).sum( ) /count[idxs].sum())
-        # wav_removed.append((removed[idxs] * count[idxs]).sum( ) /count[idxs].sum())
+        # ax.hist(degree, )
+        ax = fig.add_subplot(3, 3, rc + 1)
+        ax.bar(unique_degrees, count_degrees)
+        ax.set_xlabel('degree per node')
+        ax.set_ylabel('frequency')
+        ax.set_title(f"target degree {k}")
+    ax.set_title('distribution of node degrees')
+    plt.show()
 
-#     bar_df = pd.DataFrame(index=bin_labels,
-#                           data={'retained': wav_retained,
-#                                 'added': wav_added,
-#                                 'removed': wav_removed})
-#     ax=fig.add_subplot(3 ,3 ,rc+1)
-#     bar_df.plot(kind="bar", stacked=True, ax=ax)
-#     ax.set_title(f"target degree {i}")
-# plt.show()
+
+if __name__ == "__main__":
+  parser = argparse.ArgumentParser()
+
+  parser.add_argument('--use_cora_defaults', action='store_true',
+                      help='Whether to run with best params for cora. Overrides the choice of dataset')
+  # data args
+  parser.add_argument('--dataset', type=str, default='Cora',
+                      help='Cora, Citeseer, Pubmed, Computers, Photo, CoauthorCS')
+  parser.add_argument('--data_norm', type=str, default='rw',
+                      help='rw for random walk, gcn for symmetric gcn norm')
+  parser.add_argument('--self_loop_weight', type=float, default=1.0, help='Weight of self-loops.')
+  parser.add_argument('--use_labels', dest='use_labels', action='store_true', help='Also diffuse labels')
+  parser.add_argument('--label_rate', type=float, default=0.5, help='% of training labels to use when --use_labels is set.')
+  # GNN args
+  parser.add_argument('--hidden_dim', type=int, default=16, help='Hidden dimension.')
+  parser.add_argument('--fc_out', dest='fc_out', action='store_true', help='Add a fully connected layer to the decoder.')
+  parser.add_argument('--input_dropout', type=float, default=0.5, help='Input dropout rate.')
+  parser.add_argument('--dropout', type=float, default=0.0, help='Dropout rate.')
+  parser.add_argument("--batch_norm", dest='batch_norm', action='store_true', help='search over reg params')
+  parser.add_argument('--optimizer', type=str, default='adam', help='One from sgd, rmsprop, adam, adagrad, adamax.')
+  parser.add_argument('--lr', type=float, default=0.01, help='Learning rate.')
+  parser.add_argument('--decay', type=float, default=5e-4, help='Weight decay for optimization')
+  parser.add_argument('--epoch', type=int, default=100, help='Number of training epochs per iteration.')
+  parser.add_argument('--alpha', type=float, default=1.0, help='Factor in front matrix A.')
+  parser.add_argument('--alpha_dim', type=str, default='sc', help='choose either scalar (sc) or vector (vc) alpha')
+  parser.add_argument('--no_alpha_sigmoid', dest='no_alpha_sigmoid', action='store_true',
+                      help='apply sigmoid before multiplying by alpha')
+  parser.add_argument('--beta_dim', type=str, default='sc', help='choose either scalar (sc) or vector (vc) beta')
+  parser.add_argument('--block', type=str, default='constant', help='constant, mixed, attention, hard_attention, SDE')
+  parser.add_argument('--function', type=str, default='laplacian', help='laplacian, transformer, dorsey, GAT, SDE')
+  # ODE args
+  parser.add_argument('--time', type=float, default=1.0, help='End time of ODE integrator.')
+  parser.add_argument('--augment', action='store_true',
+                      help='double the length of the feature vector by appending zeros to stabilist ODE learning')
+  parser.add_argument('--method', type=str, default='dopri5',
+                      help="set the numerical solver: dopri5, euler, rk4, midpoint")
+  parser.add_argument('--step_size', type=float, default=1,
+                      help='fixed step size when using fixed step solvers e.g. rk4')
+  parser.add_argument('--max_iters', type=float, default=100, help='maximum number of integration steps')
+  parser.add_argument(
+    "--adjoint_method", type=str, default="adaptive_heun",
+    help="set the numerical solver for the backward pass: dopri5, euler, rk4, midpoint"
+  )
+  parser.add_argument('--adjoint', dest='adjoint', action='store_true',
+                      help='use the adjoint ODE method to reduce memory footprint')
+  parser.add_argument('--adjoint_step_size', type=float, default=1,
+                      help='fixed step size when using fixed step adjoint solvers e.g. rk4')
+  parser.add_argument('--tol_scale', type=float, default=1., help='multiplier for atol and rtol')
+  parser.add_argument("--tol_scale_adjoint", type=float, default=1.0,
+                      help="multiplier for adjoint_atol and adjoint_rtol")
+  parser.add_argument('--ode_blocks', type=int, default=1, help='number of ode blocks to run')
+  parser.add_argument('--add_source', dest='add_source', action='store_true',
+                      help='If try get rid of alpha param and the beta*x0 source term')
+  # SDE args
+  parser.add_argument('--dt_min', type=float, default=1e-5, help='minimum timestep for the SDE solver')
+  parser.add_argument('--dt', type=float, default=1e-3, help='fixed step size')
+  parser.add_argument('--adaptive', dest='adaptive', action='store_true', help='use adaptive step sizes')
+  # Attention args
+  parser.add_argument('--leaky_relu_slope', type=float, default=0.2,
+                      help='slope of the negative part of the leaky relu used in attention')
+  parser.add_argument('--attention_dropout', type=float, default=0., help='dropout of attention weights')
+  parser.add_argument('--heads', type=int, default=4, help='number of attention heads')
+  parser.add_argument('--attention_norm_idx', type=int, default=0, help='0 = normalise rows, 1 = normalise cols')
+  parser.add_argument('--attention_dim', type=int, default=64,
+                      help='the size to project x to before calculating att scores')
+  parser.add_argument('--mix_features', dest='mix_features', action='store_true',
+                      help='apply a feature transformation xW to the ODE')
+  parser.add_argument("--max_nfe", type=int, default=1000,
+                      help="Maximum number of function evaluations in an epoch. Stiff ODEs will hang if not set.")
+  parser.add_argument('--reweight_attention', dest='reweight_attention', action='store_true',
+                      help="multiply attention scores by edge weights before softmax")
+  # regularisation args
+  parser.add_argument('--jacobian_norm2', type=float, default=None, help="int_t ||df/dx||_F^2")
+  parser.add_argument('--total_deriv', type=float, default=None, help="int_t ||df/dt||^2")
+
+  parser.add_argument('--kinetic_energy', type=float, default=None, help="int_t ||f||_2^2")
+  parser.add_argument('--directional_penalty', type=float, default=None, help="int_t ||(df/dx)^T f||^2")
+
+  # rewiring args
+  parser.add_argument('--rewiring', type=str, default=None, help="two_hop, gdc")
+  parser.add_argument('--gdc_method', type=str, default='ppr', help="ppr, heat, coeff")
+  parser.add_argument('--gdc_sparsification', type=str, default='topk', help="threshold, topk")
+  parser.add_argument('--gdc_k', type=int, default=64, help="number of neighbours to sparsify to when using topk")
+  parser.add_argument('--gdc_threshold', type=float, default=0.0001,
+                      help="obove this edge weight, keep edges when using threshold")
+  parser.add_argument('--gdc_avg_degree', type=int, default=64,
+                      help="if gdc_threshold is not given can be calculated by specifying avg degree")
+  parser.add_argument('--ppr_alpha', type=float, default=0.05, help="teleport probability")
+  parser.add_argument('--heat_time', type=float, default=3., help="time to run gdc heat kernal diffusion for")
+  parser.add_argument('--att_samp_pct', type=float, default=1,
+                      help="float in [0,1). The percentage of edges to retain based on attention scores")
+  parser.add_argument('--use_flux', dest='use_flux', action='store_true',
+                      help='incorporate the feature grad in attention based edge dropout')
+  parser.add_argument("--exact", action="store_true",
+                      help="for small datasets can do exact diffusion. If dataset is too big for matrix inversion then you can't")
+  parser.add_argument('--M_nodes', type=int, default=64, help="new number of nodes to add")
+  parser.add_argument('--new_edges', type=str, default="random", help="random, random_walk, k_hop")
+  parser.add_argument('--sparsify', type=str, default="S_hat", help="S_hat, recalc_att")
+  parser.add_argument('--threshold_type', type=str, default="addD_rvR", help="topk_adj, addD_rvR")
+  parser.add_argument('--rw_addD', type=float, default=0.02, help="percentage of new edges to add")
+  parser.add_argument('--rw_rmvR', type=float, default=0.02, help="percentage of edges to remove")
+
+
+  args = parser.parse_args()
+  opt = vars(args)
+  main(opt)
