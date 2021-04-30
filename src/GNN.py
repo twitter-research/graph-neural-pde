1823413dd100ee12d28690081cc2d965a59c9cb6
James Rowbottom <jrowbottom@twitter.com>
2021-04-30 13:26:31 +0100

    removed mlp use in encoder by default.

--- src/GNN.py
+++ src/GNN.py
@@ -21,8 +21,9 @@ class GNN(BaseGNN):
       x = x[:, :self.num_features]
     x = F.dropout(x, self.opt['input_dropout'], training=self.training)
     x = F.dropout(self.m1(x), self.opt['dropout'], training=self.training)
-    x = F.dropout(x + self.m11(F.relu(x)), self.opt['dropout'], training=self.training)
-    x = F.dropout(x + self.m12(F.relu(x)), self.opt['dropout'], training=self.training)
+    if self.opt['use_mlp']:
+      x = F.dropout(x + self.m11(F.relu(x)), self.opt['dropout'], training=self.training)
+      x = F.dropout(x + self.m12(F.relu(x)), self.opt['dropout'], training=self.training)
     # todo investigate if some input non-linearity solves the problem with smooth deformations identified in the ANODE paper
     # if True:
     #   x = F.relu(x)
