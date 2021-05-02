2f37bb23b250e0386cdefb31382a1635e779d09a
James Rowbottom <jrowbottom@twitter.com>
2021-05-02 13:10:24 +0100

    removing duplicate rewiring args to ray tune

--- src/graph_rewiring_ray.py
+++ src/graph_rewiring_ray.py
@@ -484,10 +484,6 @@ if __name__ == "__main__":
                         help="for small datasets can do exact diffusion. If dataset is too big for matrix inversion then you can't")
     parser.add_argument('--att_samp_pct', type=float, default=1,
                         help="float in [0,1). The percentage of edges to retain based on attention scores")
-    parser.add_argument('--use_flux', dest='use_flux', action='store_true',
-                        help='incorporate the feature grad in attention based edge dropout')
-    parser.add_argument("--exact", action="store_true",
-                        help="for small datasets can do exact diffusion. If dataset is too big for matrix inversion then you can't")
     parser.add_argument('--M_nodes', type=int, default=64, help="new number of nodes to add")
     parser.add_argument('--new_edges', type=str, default="random", help="random, random_walk, k_hop")
     parser.add_argument('--sparsify', type=str, default="S_hat", help="S_hat, recalc_att")
