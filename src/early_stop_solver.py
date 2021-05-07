15987eea5594aaefc04b6917d5578f809bb00ef4
Ben Chamberlain <bchamberlain@twitter.com>
2021-05-07 19:18:32 +0100

    bugfix

--- src/early_stop_solver.py
+++ src/early_stop_solver.py
@@ -280,7 +280,7 @@ class EarlyStopInt(torch.nn.Module):
     if int(ver[0]+ver[2]+ver[4]) >= 21: #0.2.1 change of signiture *around* this release
       shapes, func, y0, t, rtol, atol, method, options, event_fn, t_is_reversed = _check_inputs(func, y0, self.t, rtol, atol, method, options, event_fn, SOLVERS)
     else:
-      shapes, func, y0, t, rtol, atol, method, options = _check_inputs(func, y0, self.t, rtol, atol, method, options, event_fn, SOLVERS)
+      shapes, func, y0, t, rtol, atol, method, options = _check_inputs(func, y0, self.t, rtol, atol, method, options, SOLVERS)
 
     self.solver = SOLVERS[method](func, y0, rtol=rtol, atol=atol, opt=self.opt, **options)
     if self.solver.data is None:
