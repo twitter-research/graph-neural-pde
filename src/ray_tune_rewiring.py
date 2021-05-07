14dfbd3527bce8b74bd6cacc278f689963954591
James Rowbottom <jrowbottom@twitter.com>
2021-05-08 00:11:08 +0100

    apply_beltrami to device

--- src/ray_tune_rewiring.py
+++ src/ray_tune_rewiring.py
@@ -55,7 +55,7 @@ def train_ray_rand(opt, checkpoint_dir=None, data_dir="../data"):
     # datas.append(dataset.data)
 
     if opt['beltrami']:
-      dataset.data.x = apply_beltrami(dataset.data, opt, device, type="pos_encoding")
+      dataset.data.x = apply_beltrami(dataset.data, opt, device, type="pos_encoding").to(device)
 
     data = dataset.data.to(device)
     datas.append(dataset.data)
