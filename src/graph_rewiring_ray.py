c4d6900befe8970ab056a3c65fa79fed98ce64e6
Ben Chamberlain <bchamberlain@twitter.com>
2021-05-16 13:40:09 +0100

    updated Photo search space

--- src/graph_rewiring_ray.py
+++ src/graph_rewiring_ray.py
@@ -344,21 +344,22 @@ def set_pubmed_search_space(opt):
 
 
 def set_photo_search_space(opt):
-  opt["decay"] = tune.loguniform(0.001, 1e-2)
+  opt["decay"] = tune.loguniform(0.0001, 1e-2)
   if opt['regularise']:
     opt["kinetic_energy"] = tune.loguniform(0.01, 5.0)
     opt["directional_penalty"] = tune.loguniform(0.001, 10.0)
 
   opt["hidden_dim"] = tune.sample_from(lambda _: 2 ** np.random.randint(3, 7))
-  opt["lr"] = tune.loguniform(1e-3, 0.1)
+  opt["lr"] = tune.loguniform(5e-4, 0.1)
   opt["input_dropout"] = tune.uniform(0.4, 0.8)
-  opt["dropout"] = tune.uniform(0, 0.8)
-  opt["time"] = tune.uniform(0.5, 7.0)
-  opt["optimizer"] = tune.choice(["adam", "adamax", "rmsprop"])
+  opt["dropout"] = tune.uniform(0, 0.5)
+  opt["time"] = tune.uniform(0.5, 12.0)
+  # opt["optimizer"] = tune.choice(["adam", "adamax", "rmsprop"])
+  opt["optimizer"] = "adam"
 
   if opt["block"] in {'attention', 'mixed'} or opt['function'] in {'GAT', 'transformer', 'dorsey'}:
     opt["heads"] = tune.sample_from(lambda _: 2 ** np.random.randint(0, 3))
-    opt["attention_dim"] = tune.sample_from(lambda _: 2 ** np.random.randint(3, 6))
+    opt["attention_dim"] = tune.sample_from(lambda _: 2 ** np.random.randint(3, 7))
     opt['attention_norm_idx'] = tune.choice([0, 1])
     opt["self_loop_weight"] = tune.choice([0, 0.5, 1, 2]) if opt['block'] == 'mixed' else tune.choice(
       [0, 1])
@@ -370,7 +371,7 @@ def set_photo_search_space(opt):
 
   if opt["adjoint"]:
     opt["tol_scale_adjoint"] = tune.loguniform(100, 1e5)
-    opt["adjoint_method"] = tune.choice(["dopri5", "adaptive_heun"])
+    opt["adjoint_method"] = tune.choice(["dopri5", "adaptive_heun", "rk4"])
 
   if opt['rewiring'] == 'gdc':
     # opt['gdc_sparsification'] = tune.choice(['topk', 'threshold'])
@@ -385,9 +386,11 @@ def set_photo_search_space(opt):
     # opt['heat_time'] = tune.uniform(1, 5)
 
   opt['add_source'] = tune.choice([True, False])
+  opt['add_source'] = False
   # opt['att_samp_pct'] = tune.uniform(0.3, 1)
-  opt['batch_norm'] = tune.choice([True, False])
-  # opt['use_mlp'] = tune.choice([True, False])
+  # opt['batch_norm'] = tune.choice([True, False])
+  opt['batch_norm'] = True
+  opt['use_mlp'] = tune.choice([True, False])
 
   return opt
 
