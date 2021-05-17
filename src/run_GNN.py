7e52c0b784ab2c76fd6e60d781a3459cb2c43fe3
James Rowbottom <jrowbottom@twitter.com>
2021-05-17 19:53:10 +0100

    add to run_GNN.py parser.add_argument("--not_lcc", action="store_false", help="don't use the largest connected component")

--- src/run_GNN.py
+++ src/run_GNN.py
@@ -388,6 +388,7 @@ if __name__ == '__main__':
                       help="if gdc_threshold is not given can be calculated by specifying avg degree")
   parser.add_argument('--ppr_alpha', type=float, default=0.05, help="teleport probability")
   parser.add_argument('--heat_time', type=float, default=3., help="time to run gdc heat kernal diffusion for")
+  parser.add_argument("--not_lcc", action="store_false", help="don't use the largest connected component")
   parser.add_argument('--att_samp_pct', type=float, default=1,
                       help="float in [0,1). The percentage of edges to retain based on attention scores")
   parser.add_argument('--use_flux', dest='use_flux', action='store_true',
