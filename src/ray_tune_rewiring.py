298da8545e30ad866cbf712e0f7826601e957434
James Rowbottom <jrowbottom@twitter.com>
2021-05-23 15:58:18 +0100

    Pubmed Planetoid splits tune

--- src/ray_tune_rewiring.py
+++ src/ray_tune_rewiring.py
@@ -352,6 +352,7 @@ def mainLoop(opt):
   opt['rewiring'] = 'None' #'gdc' #None
   # opt['attention_type'] = 'exp_kernel_z' #not QK space ie 'exp_kernel'
   opt['beltrami'] = True
+  opt['adjoint'] = True
 
   opt['rewire_KNN'] = False
   opt['edge_sampling'] = False
