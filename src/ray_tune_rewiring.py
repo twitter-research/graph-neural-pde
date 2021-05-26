b5c0025211424eaf91948dad64d6ebf27ecbeec5
James Rowbottom <jrowbottom@twitter.com>
2021-05-26 20:05:48 +0100

    ES_test after importance fixes

--- src/ray_tune_rewiring.py
+++ src/ray_tune_rewiring.py
@@ -591,14 +591,16 @@ def ES_test(opt):
 
       best_params_ret['edge_sampling_online'] = True
       best_params_ret['edge_sampling_add_type'] = tune.choice(['importance', 'random'])
-      opt['edge_sampling_space'] = 'attention' #tune.choice(['attention', 'pos_distance', 'z_distance'])
-      opt['edge_sampling_online_reps'] = 3 #tune.choice([2, 3, 4])
-      # opt['edge_sampling_sym'] = False #tune.choice([True, False])
-      opt['edge_sampling_add'] = tune.choice([0.04, 0.08, 0.16, 0.32, 0.64])  # tune.choice([0.04, 0.08, 0.16, 0.32])
-      opt['edge_sampling_rmv'] = tune.choice([0.0, 0.04, 0.08])  # tune.choice([0.04, 0.08, 0.16, 0.32])
-      # opt["time"] = tune.uniform(0.25, 5.0)
+      best_params_ret['edge_sampling_space'] = 'attention' #tune.choice(['attention', 'pos_distance', 'z_distance'])
+      best_params_ret['edge_sampling_online_reps'] = 3 #tune.choice([2, 3, 4])
+      # best_params_ret['edge_sampling_sym'] = False #tune.choice([True, False])
+      best_params_ret['edge_sampling_add'] = tune.choice([0.04, 0.08, 0.16, 0.32, 0.64])  # tune.choice([0.04, 0.08, 0.16, 0.32])
+      best_params_ret['edge_sampling_rmv'] = tune.choice([0.0, 0.04, 0.08])  # tune.choice([0.04, 0.08, 0.16, 0.32])
+      best_params_ret["time"] = best_params_ret["time"] / 3
 
-      best_params_ret = edge_sampling_online_space(best_params_ret)
+      # best_params_ret["time"] = tune.uniform(0.25, 5.0)
+
+      # best_params_ret = edge_sampling_online_space(best_params_ret)
 
       try:
         best_params_ret['mix_features']
