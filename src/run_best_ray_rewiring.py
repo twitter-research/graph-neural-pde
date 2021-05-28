6c94417d614bd709a96be2ecbec70d2a6d57ece8
James Rowbottom <jrowbottom@twitter.com>
2021-05-28 15:35:18 +0100

    Final Citeseer/Photo ablation

--- src/run_best_ray_rewiring.py
+++ src/run_best_ray_rewiring.py
@@ -779,7 +779,7 @@ if __name__ == '__main__':
   args = parser.parse_args()
 
   opt = vars(args)
-  run_best_params(opt)
+  # run_best_params(opt)
   # mainLoop(opt)
   # KNN_abalation(opt)
   # KNN_abalation_grid(opt)
