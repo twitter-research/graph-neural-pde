8e135c38a94ebf0febb65957c2b5af32d3dc0880
James Rowbottom <jrowbottom@twitter.com>
2021-05-28 14:07:40 +0100

    Citeseer_DW_rewiring3

--- src/ray_tune_rewiring.py
+++ src/ray_tune_rewiring.py
@@ -652,8 +652,8 @@ def pos_enc_DW_tune(opt):
   opt['edge_sampling'] = False
   opt['fa_layer'] = False
 
-  datas = ['Photo']#['Cora', 'Citeseer', 'Photo', 'Computers', 'CoauthorCS', 'Pubmed']
-  folders = ['Photo_DW_rewiring4'] #['Cora_DW_rewiring','Citeseer_DW_rewiring','Photo_DW_rewiring',
+  datas = ['Citeseer'] #['Photo']#['Cora', 'Citeseer', 'Photo', 'Computers', 'CoauthorCS', 'Pubmed']
+  folders = ['Citeseer_DW_rewiring3'] #['Photo_DW_rewiring4'] #['Cora_DW_rewiring','Citeseer_DW_rewiring','Photo_DW_rewiring',
               # 'Computers_DW_rewiring','CoauthorCS_DW_rewiring','Pubmed_DW_rewiring']
   for i, ds in enumerate(datas):
     print(f"Running Tuning for {ds}")
