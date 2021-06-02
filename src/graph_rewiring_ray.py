8ea7de5095c89f046122e86c73f808139ccbaf7f
James Rowbottom <jrowbottom@twitter.com>
2021-06-02 16:41:30 +0100

    opt = {**cmd_opt, **best_opt}

--- src/graph_rewiring_ray.py
+++ src/graph_rewiring_ray.py
@@ -544,7 +544,7 @@ def set_arxiv_search_space(opt):
 def set_search_space(cmd_opt):
   # opt = set_rewiring_space(opt)
   best_opt = best_params_dict[cmd_opt['dataset']]
-  opt = {**best_opt, **cmd_opt}
+  opt = {**cmd_opt, **best_opt}
 
   opt['name'] = 'Cora_time_tune'
   if opt["dataset"] == "Cora":
