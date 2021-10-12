01b5056f9923ab7c70f3321bac2c357936f3a094
James Rowbottom <jr1419@ic.ac.uk>
2021-10-12 16:13:59 +0100

    ablations_plotting_code.py

--- src/ablations_plotting_code.py
+++ src/ablations_plotting_code.py
@@ -1,3 +1,7 @@
+import matplotlib
+matplotlib.rcParams['pdf.fonttype'] = 42
+matplotlib.rcParams['ps.fonttype'] = 42
+
 import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
 import numpy as np
@@ -26,6 +30,7 @@ def fig1a():
     # Add some text for labels, title and custom x-axis tick labels, etc.
     ax.set_xlabel('Datasets')
     ax.set_ylabel('Accuracy')
+    ax.grid(axis='y')
     # ax.set_title('')
     ax.set_xticks(x)
     ax.set_xticklabels(datasets)
@@ -68,6 +73,8 @@ def fig1b():
     # Add some text for labels, title and custom x-axis tick labels, etc.
     ax.set_xlabel('Datasets')
     ax.set_ylabel('Accuracy')
+    ax.grid(axis='y')
+
     # ax.set_title('')
     ax.set_xticks(x)
     ax.set_xticklabels(datasets, fontsize=10)
@@ -81,11 +88,13 @@ def fig1b():
     ax.bar_label(rects3, padding=0, fontsize=fs, color=sns.color_palette()[2])
 
     fig.tight_layout()
+    plt.savefig('../ablations/Neurips_plots/w_wo_pos_encs_abl.pdf')
     plt.show()
 
 
 # Euclidean and hyperbolic positional embeddings
 def fig2():
+    # LHS
     datasets = ['Cora','CiteSeer','PubMed','Coauthor','CS Computer','Photo']
     x = np.arange(len(datasets))
 
@@ -106,6 +115,7 @@ def fig2():
     # Add some text for labels, title and custom x-axis tick labels, etc.
     ax.set_xlabel('Datasets')
     ax.set_ylabel('Accuracy')
+    ax.grid(axis='y')
     # ax.set_title('')
     ax.set_xticks(x)
     ax.set_xticklabels(datasets)
@@ -118,16 +128,212 @@ def fig2():
 
     fig.tight_layout()
     plt.show()
-
-
-
-
+    #RHS - might not be needed
+    #build_ablation_fig.ipynb
+    #'../ablations/Neurips_plots/pos_emb_dim.pdf'
 
 #Step size ablation
 def fig3():
-    pass
+
+    x_labels = ['Dopri5', '0.25', '0.5', '1', '2', '4', '8']
+    x = np.arange(len(x_labels))
+
+    mean = {
+    'Cora':	[83.8, 84.3, 83.4, 83.5, 84.3, 40.9, 35.1],
+    'Citeseer':	[74.3, 73.1, 74.2, 73.3, 74.9, 59.9, 23.3],
+    'Pubmed':	[80.0, 78.8, 79.8, 79.2, 78.6, 72.4, 46.8],
+    'CoauthorCS':	[92.8, 92.8, 92.7, 92.5, 91.6, 77.1, 77.1],
+    'Computers':	[85.1, 84.8, 85.0, 85.0, 84.8, 75.8, 75.2],
+    'Photo':	[92.8, 92.9, 92.6, 92.3, 91.7, 82.7, 83.2]}
+
+    std = {
+    'Cora':	[1.3, 0.8, 1.1, 1.7, 1.1, 3.1, 2.8],
+    'Citeseer':	[2.3, 1.8, 1.6, 2.2, 1.1, 3.8, 3.2],
+    'Pubmed':	[1.1, 1.4, 1.5, 1.3, 1.9, 2.5, 1.9],
+    'CoauthorCS':	[0.1, 0.2, 0.3, 0.1, 0.1, 0.3, 0.4],
+    'Computers':	[0.5, 0.6, 0.6, 0.8, 0.4, 1.1, 0.8],
+    'Photo':	[0.4, 0.3, 0.2, 0.3, 0.5, 0.8, 0.8]}
+
+    sns.set()
+    sns.set_style("ticks")
+
+    fig, (ax, ax2) = plt.subplots(1, 2, sharex=True)
+
+    # plot the same data on both axes
+    for i, (k, v) in enumerate(mean.items()):
+        ax.plot(x, v, label=k)
+        ax2.plot(x, v, label=k)
+
+    # ax.set_xticks(x)
+    ax.set_xticklabels(x_labels)
+
+
+    ax.set_xlim(0, 0.25)
+    ax2.set_xlim(0.75, 8)
+
+    # hide the spines between ax and ax2
+    ax.spines['right'].set_visible(False)
+    ax2.spines['left'].set_visible(False)
+    ax.yaxis.tick_left()
+    ax.tick_params(labelright=False)
+    ax2.tick_params(bottom=True, top=False, left=False, right=False)
+    ax2.tick_params(labelbottom=True, labeltop=False, labelleft=False, labelright=False)
+    ax2.yaxis.tick_right()
+
+    d = .015  # how big to make the diagonal lines in axes coordinates
+    # arguments to pass plot, just so we don't keep repeating them
+    kwargs = dict(transform=ax.transAxes, color='k', clip_on=False)
+    ax.plot((1 - d, 1 + d), (-d, +d), **kwargs)
+    ax.plot((1 - d, 1 + d), (1 - d, 1 + d), **kwargs)
+
+    kwargs.update(transform=ax2.transAxes)  # switch to the bottom axes
+    ax2.plot((-d, +d), (1 - d, 1 + d), **kwargs)
+    ax2.plot((-d, +d), (-d, +d), **kwargs)
+
+    plt.show()
+
+
+def fig4():
+
+    x_labels = ['Dopri5', '0.25', '0.5', '1', '2', '4', '8']
+    x = np.arange(len(x_labels))
+
+    mean = {
+    'Cora':	[83.8, 84.3, 83.4, 83.5, 84.3, 40.9, 35.1],
+    'Citeseer':	[74.3, 73.1, 74.2, 73.3, 74.9, 59.9, 23.3],
+    'Pubmed':	[80.0, 78.8, 79.8, 79.2, 78.6, 72.4, 46.8],
+    'CoauthorCS':	[92.8, 92.8, 92.7, 92.5, 91.6, 77.1, 77.1],
+    'Computers':	[85.1, 84.8, 85.0, 85.0, 84.8, 75.8, 75.2],
+    'Photo':	[92.8, 92.9, 92.6, 92.3, 91.7, 82.7, 83.2]}
+
+    std = {
+    'Cora':	[1.3, 0.8, 1.1, 1.7, 1.1, 3.1, 2.8],
+    'Citeseer':	[2.3, 1.8, 1.6, 2.2, 1.1, 3.8, 3.2],
+    'Pubmed':	[1.1, 1.4, 1.5, 1.3, 1.9, 2.5, 1.9],
+    'CoauthorCS':	[0.1, 0.2, 0.3, 0.1, 0.1, 0.3, 0.4],
+    'Computers':	[0.5, 0.6, 0.6, 0.8, 0.4, 1.1, 0.8],
+    'Photo':	[0.4, 0.3, 0.2, 0.3, 0.5, 0.8, 0.8]}
+
+    sns.set()
+    sns.set_style("ticks")
+
+    fig, ax = plt.subplots()
+
+    # plot the same data on both axes
+    for i, (k, v) in enumerate(mean.items()):
+        ax.plot(x, v, label=k)
+
+    ax.set_xticks(x)
+    ax.set_xticklabels(x_labels)
+    ax.legend(loc='best', ncol=2)
+
+    ax.set_xlabel('Stepsize')
+    ax.set_ylabel('Accuracy')
+
+    plt.show()
+
+
+def fig5():
+    from brokenaxes import brokenaxes
+
+    x_labels = ['Dopri5', '0.25', '0.5', '1', '2', '4', '8']
+    x = np.arange(len(x_labels))
+
+    mean = {
+        'Cora': [83.8, 84.3, 83.4, 83.5, 84.3, 40.9, 35.1],
+        'Citeseer': [74.3, 73.1, 74.2, 73.3, 74.9, 59.9, 23.3],
+        'Pubmed': [80.0, 78.8, 79.8, 79.2, 78.6, 72.4, 46.8],
+        'CoauthorCS': [92.8, 92.8, 92.7, 92.5, 91.6, 77.1, 77.1],
+        'Computers': [85.1, 84.8, 85.0, 85.0, 84.8, 75.8, 75.2],
+        'Photo': [92.8, 92.9, 92.6, 92.3, 91.7, 82.7, 83.2]}
+
+    std = {
+        'Cora': [1.3, 0.8, 1.1, 1.7, 1.1, 3.1, 2.8],
+        'Citeseer': [2.3, 1.8, 1.6, 2.2, 1.1, 3.8, 3.2],
+        'Pubmed': [1.1, 1.4, 1.5, 1.3, 1.9, 2.5, 1.9],
+        'CoauthorCS': [0.1, 0.2, 0.3, 0.1, 0.1, 0.3, 0.4],
+        'Computers': [0.5, 0.6, 0.6, 0.8, 0.4, 1.1, 0.8],
+        'Photo': [0.4, 0.3, 0.2, 0.3, 0.5, 0.8, 0.8]}
+
+    sns.set()
+    sns.set_style("ticks")
+
+    fig, ax = plt.subplots()
+    # fig = plt.figure(figsize=(6, 4))
+    baxes = brokenaxes(xlims=((0, 0.25), (0.25, 7)), hspace=.05)
+
+    # plot the same data on both axes
+    for i, (k, v) in enumerate(mean.items()):
+        baxes.plot(x, v, label=k)
+
+    ax.set_xticks(x)
+    ax.set_xticklabels(x_labels)
+    ax.legend(loc='best', ncol=2)
+
+    ax.set_xlabel('Stepsize')
+    ax.set_ylabel('Accuracy')
+
+    plt.show()
+
+def fig6():
+
+    x_labels = ['Dopri5', '0.25', '0.5', '1', '2', '4', '8']
+    x = np.arange(len(x_labels))
+
+    means = {
+    'Cora':	[83.8, 84.3, 83.4, 83.5, 84.3, 40.9, 35.1],
+    'Citeseer':	[74.3, 73.1, 74.2, 73.3, 74.9, 59.9, 23.3],
+    'Pubmed':	[80.0, 78.8, 79.8, 79.2, 78.6, 72.4, 46.8],
+    'CoauthorCS':	[92.8, 92.8, 92.7, 92.5, 91.6, 77.1, 77.1],
+    'Computers':	[85.1, 84.8, 85.0, 85.0, 84.8, 75.8, 75.2],
+    'Photo':	[92.8, 92.9, 92.6, 92.3, 91.7, 82.7, 83.2]}
+
+    stds = {
+    'Cora':	[1.3, 0.8, 1.1, 1.7, 1.1, 3.1, 2.8],
+    'Citeseer':	[2.3, 1.8, 1.6, 2.2, 1.1, 3.8, 3.2],
+    'Pubmed':	[1.1, 1.4, 1.5, 1.3, 1.9, 2.5, 1.9],
+    'CoauthorCS':	[0.1, 0.2, 0.3, 0.1, 0.1, 0.3, 0.4],
+    'Computers':	[0.5, 0.6, 0.6, 0.8, 0.4, 1.1, 0.8],
+    'Photo':	[0.4, 0.3, 0.2, 0.3, 0.5, 0.8, 0.8]}
+
+    sns.set()
+    sns.set_style("ticks")
+
+    fig, ax = plt.subplots()
+
+    # plot the same data on both axes
+    for i, (k, v) in enumerate(means.items()):
+        ax.scatter(x[0], v[0], marker='x')
+        ax.plot(x[1:], np.array(v[1:]), label=k, c=sns.color_palette()[i])
+        ax.plot(x[:2], np.array(v[:2]), c=sns.color_palette()[i], linestyle='--')
+
+        # mean = np.array(v[1:])
+        # std = np.array(stds[k][1:])
+        # ax.fill_between(x[1:], mean - std, mean + std, alpha=0.3, facecolor=sns.color_palette()[i])
+
+        mean = np.array(v)
+        std = np.array(stds[k])
+        ax.fill_between(x, mean - std, mean + std, alpha=0.3, facecolor=sns.color_palette()[i])
+
+
+    ax.set_xticks(x)
+    ax.set_xticklabels(x_labels)
+    ax.legend(loc='best', ncol=2)
+    ax.grid(axis='y')
+    ax.set_xlabel('Stepsize')
+    ax.set_ylabel('Accuracy')
+    plt.savefig('../ablations/Neurips_plots/step_size_abl.pdf')
+
+    plt.show()
 
 if __name__ == "__main__":
     # fig1a()
-    # fig1b()
-    fig2()
+    fig1b()
+    # fig2()
+    # fig3()
+    # fig4()
+    # fig5()
+    fig6()
+
+    # broken_X()
+
