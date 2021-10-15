f9948c593d2163b8025ace821777c5b14b6fd26f
James Rowbottom <jr1419@ic.ac.uk>
2021-10-15 14:46:25 +0100

    BLEND_slides_karate.py

--- src/BLEND_slides_animation.py
+++ src/BLEND_slides_animation.py
@@ -11,30 +11,46 @@ from torch_geometric.data import Data
 
 
 # loop through data and update plot
-def update(ii, pos_t, x_t, ax, NXgraph):
+def update(ii, pos_t, x_t, ax, NXgraph, params):
   plt.tight_layout()
+  plt.xlim([-5, 5])
+  plt.xlim([-5, 5])
   pos_0 = pos_t[:, :, 0].detach().numpy()
   ax.clear()
   x_i = x_t[:, ii].detach().numpy()
   pos_i = pos_t[:, :, ii].detach().numpy()
   pos_i_dict = {i: pos_i[i, :].tolist() for i in range(pos_0.shape[0])}
-  # ax.set_xlim([-5, 5])
-  # ax.set_xlim([-5, 5])
-  nx.draw(NXgraph, pos=pos_i_dict, ax=ax, node_size=300 / 4,
-          node_color=x_i, cmap=plt.get_cmap('Spectral'))
 
-  plt.title(f"t={ii}  Beltrami Flow")
+  ax.set_xlim([-5, 5])
+  ax.set_xlim([-5, 5])
+  nx.draw(NXgraph, pos=pos_i_dict, ax=ax, node_size=params['node_size'],
+          node_color=x_i, cmap=plt.get_cmap('Spectral'), arrows=False, width=0.25) #=params['edge_with'] )
+
+  limits = plt.axis('on')
+  ax.patch.set_edgecolor('black')
+  ax.patch.set_linewidth('1')
+  ax.tick_params(left=True, bottom=True, labelleft=True, labelbottom=True)
+  # ax.set_xticks([])
+  # ax.set_yticks([])
+  plt.title(f"Beltrami Flow, diffusion time={ii//10}", fontsize=16)
 
 
-def create_animation(x_t, pos_t, NXgraph, filepath, im_height=16, im_width=16, frames=3, fps=1.5):
+
+def create_animation(x_t, pos_t, NXgraph, params, filepath):
   # # draw initial graph
   # time = 0
   # x_0 = x_t[:, time].detach().numpy()
   # pos_0 = pos_t[:, :, time].detach().numpy()
   # pos_0_dict = {i: pos[i, :].tolist() for i in range(pos_0.shape[0])}
-  fig, ax = plt.subplots()
-  ax.set_xlim([-5, 5])
-  ax.set_xlim([-5, 5])
+
+  fig, ax = plt.subplots(figsize=(params['im_width'], params['im_height']))
+  # ax.set_xlim([-5, 5])
+  # ax.set_xlim([-5, 5])
+  # limits = plt.axis('on')
+  # ax.tick_params(left=True, bottom=True, labelleft=True, labelbottom=True)
+  # ax.set_xlim([-5, 5])
+  # ax.set_xlim([-5, 5])
+
   # ax.axis('off')
   # nx.draw(NXgraph, pos= pos_0_dict, ax=ax, node_size=300 / 4,
   #         node_color=x, cmap=plt.get_cmap('Spectral'))
@@ -42,8 +58,8 @@ def create_animation(x_t, pos_t, NXgraph, filepath, im_height=16, im_width=16, f
   # plt.show()
 
   fig = plt.gcf()
-  animation = FuncAnimation(fig, func=update, frames=frames, fargs=(pos_t, x_t, ax, NXgraph))
-  animation.save(filepath, fps=fps)  # , writer='imagemagick', savefig_kwargs={'facecolor': 'white'}, fps=fps)
+  animation = FuncAnimation(fig, func=update, frames=params['T'], fargs=(pos_t, x_t, ax, NXgraph, params))
+  animation.save(filepath, fps=params['fps'])  #writer='imagemagick', savefig_kwargs={'facecolor': 'white'}, fps=fps)
 
 
 if __name__ == "__main__":
@@ -83,4 +99,6 @@ if __name__ == "__main__":
   frames = 3
   fps = 1.5
   filepath = f"../BLEND_animation/BLEND_animation.gif"
-  create_animation(x_t, pos_t, NXgraph, filepath, im_height, im_width, frames, fps)
+  params= {}
+  # , im_height = 16, im_width = 16, frames = 3, fps = 1.5
+  create_animation(x_t, pos_t, NXgraph, params, filepath)
--- src/BLEND_slides_karate.py
+++ src/BLEND_slides_karate.py
@@ -1,3 +1,4 @@
+import numpy as np
 import torch
 from torch_geometric.datasets import KarateClub
 from torch_geometric.data import Data
@@ -5,54 +6,98 @@ import networkx as nx
 from torch.distributions.multivariate_normal import MultivariateNormal
 from BLEND_slides_animation import create_animation
 from torch_geometric.utils.convert import to_networkx
+import matplotlib.pyplot as plt
 
 dataset = KarateClub()
 data = dataset[0]
+
+N = len(data.y)
+params = {'T':100, 'fps':10, 'end_feat_sd_scale':0.1, 'end_pos_sd_scale':0.05,
+          'node_size': 500, 'edge_width':0.25, 'im_height':8, 'im_width':16}
+
+T = params['T']
 
 new_labels = torch.tensor([0 if i in [0,1] else 1 for i in data.y.tolist()])
 # position
 pos_mean_0 = torch.tensor([-1.0, 0.0])
 pos_sd_0 = torch.tensor([[1.0, 0.0], [0.0, 1.0]])
-pos_mean_1 = torch.tensor([0.0, 1.0])
+pos_mean_1 = torch.tensor([1.0, 1.0])
 pos_sd_1 = torch.tensor([[1.0, 0.0], [0.0, 1.0]])
 MN_0 = MultivariateNormal(pos_mean_0, pos_sd_0)
 MN_1 = MultivariateNormal(pos_mean_1, pos_sd_1)
-start_pos = torch.stack([MN_0.rsample(sample_shape=torch.Size([1])) if i==0 else MN_1.rsample(sample_shape=torch.Size([1])) for i in new_labels.tolist()])
-end_pos_sd_0 = pos_sd_0 * 0.2 #torch.tensor([[1.0, 0.0], [0.0, 1.0]])
-end_pos_sd_1 = pos_sd_0 * 0.2 #torch.tensor([[1.0, 0.0], [0.0, 1.0]])
+# start_pos = torch.stack([MN_0.rsample(sample_shape=torch.Size([1])) if i==0 else MN_1.rsample(sample_shape=torch.Size([1])) for i in new_labels.tolist()])
+# start_pos = torch.stack([MN_0.sample(sample_shape=torch.Size([1])) if i==0 else MN_1.sample(sample_shape=torch.Size([1])) for i in new_labels.tolist()])
+start_pos = torch.stack([torch.tensor(np.random.multivariate_normal(pos_mean_0, pos_sd_0)) if i==0 else torch.tensor(np.random.multivariate_normal(pos_mean_1, pos_sd_1)) for i in new_labels.tolist()])
+
+end_pos_sd_0 = pos_sd_0 * params['end_pos_sd_scale'] #torch.tensor([[1.0, 0.0], [0.0, 1.0]])
+end_pos_sd_1 = pos_sd_0 * params['end_pos_sd_scale'] #torch.tensor([[1.0, 0.0], [0.0, 1.0]])
 MNp_0_end = MultivariateNormal(pos_mean_0, end_pos_sd_0)
 MNp_1_end = MultivariateNormal(pos_mean_1, end_pos_sd_1)
-end_pos = torch.stack([MNp_0_end.rsample(sample_shape=torch.Size([1])) if i==0 else MNp_1_end.rsample(sample_shape=torch.Size([1])) for i in new_labels.tolist()])
+# end_pos = torch.stack([MNp_0_end.rsample(sample_shape=torch.Size([1])) if i==0 else MNp_1_end.rsample(sample_shape=torch.Size([1])) for i in new_labels.tolist()])
+end_pos = torch.stack([MNp_0_end.sample(sample_shape=torch.Size([1])) if i==0 else MNp_1_end.sample(sample_shape=torch.Size([1])) for i in new_labels.tolist()])
 #features
 feat_mean_0 = torch.tensor([-1.0])
 feat_sd_0 = torch.tensor([1.0])
 feat_mean_1 = torch.tensor([1.0])
 feat_sd_1 = torch.tensor([1.0])
 start_feat  = torch.stack([torch.normal(feat_mean_0, feat_sd_0) if i==0 else torch.normal(feat_mean_1, feat_sd_1) for i in new_labels.tolist()])
-end_feat_sd_0 = feat_sd_0 * 0.1
-end_feat_sd_1 = feat_sd_1 * 0.1
+end_feat_sd_0 = feat_sd_0 * params['end_feat_sd_scale']
+end_feat_sd_1 = feat_sd_1 * params['end_feat_sd_scale']
 end_feat  = torch.stack([torch.normal(feat_mean_0, end_feat_sd_0) if i==0 else torch.normal(feat_mean_1, end_feat_sd_1) for i in new_labels.tolist()])
 
-N = len(data.y)
-T = 20
 # build time series with linear interp
 pos_t = torch.zeros((N,2,T)) #[34,2,20]
 feat_t = torch.zeros((N,T)) #[34,20]
+# test_start_pos_t = torch.zeros((N,2,T)) #[34,2,20]
+# test_end_pos_t = torch.zeros((N,2,T)) #[34,2,20]
+
 for t in range(T):
-    pos_t[:, :, t] = start_pos.squeeze() + (end_pos.squeeze() - start_pos.squeeze()) * t / T
-    feat_t[:, t] = start_feat.squeeze() + (end_feat.squeeze() - start_feat.squeeze()) * t / T
+    # print(end_pos.squeeze().T)
+    # print(start_pos.squeeze()[0,:])
+    # print(end_pos.squeeze()[0,:])
+    # pos_t[:, :, t] = start_pos.squeeze() + (end_pos.squeeze() - start_pos.squeeze()) * t / T
+    # feat_t[:, t] = start_feat.squeeze() + (end_feat.squeeze() - start_feat.squeeze()) * t / T
+    f = T / np.log(T)
+    s = f * np.log(1+min(t,0.8*T))
+    pos_t[:, :, t] = start_pos.squeeze() + (end_pos.squeeze() - start_pos.squeeze()) * s / T
+    feat_t[:, t] = start_feat.squeeze() + (end_feat.squeeze() - start_feat.squeeze()) * s / T
+
+    # test_start_pos_t[:, :, t] = start_pos.squeeze()
+    # test_end_pos_t[:, :, t] = end_pos.squeeze()
 
 data.new_labels = new_labels
 data.post_t = pos_t
 data.feat_t = feat_t
 data.x = start_feat.squeeze()
 
-im_height = 16
-im_width = 16
-frames = 20
-fps = 1.5
 filepath = f"../BLEND_animation/karate_animation.gif"
 
 graph = Data(x=start_feat.squeeze(), edge_index=data.edge_index)
 NXgraph = to_networkx(graph)
-create_animation(feat_t, pos_t, NXgraph, filepath, im_height, im_width, frames, fps)
+create_animation(feat_t, pos_t, NXgraph, params, filepath)
+
+#TESTS
+# fig, ax = plt.subplots()
+# plt.plot(pos_t[:,0,:].T)
+# plt.title("pos_t x")
+# plt.show()
+#
+# fig, ax = plt.subplots()
+# plt.plot(test_start_pos_t[:,0,:].T)
+# plt.title("test_start_pos_t x")
+# plt.show()
+#
+# fig, ax = plt.subplots()
+# plt.plot(test_end_pos_t[:,0,:].T)
+# plt.title("test_end_pos_t x")
+# plt.show()
+#
+# fig, ax = plt.subplots()
+# plt.plot(test_start_pos_t[:,1,:].T)
+# plt.title("test_start_pos_t y")
+# plt.show()
+#
+# fig, ax = plt.subplots()
+# plt.plot(test_end_pos_t[:,1,:].T)
+# plt.title("test_end_pos_t y")
+# plt.show()
