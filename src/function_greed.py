728320a02894320f005d8ed4b74273e1058cb808
Ben Chamberlain <bchamberlain@twitter.com>
2021-12-22 12:37:36 +0000

    passing tests for gamma and tau

--- src/function_greed.py
+++ src/function_greed.py
@@ -5,7 +5,7 @@ Implementation of the functions proposed in Graph embedding energies and diffusi
 import torch
 from torch import nn
 import torch_sparse
-import torch_scatter
+from torch_scatter import scatter_add, scatter_mul
 from torch_geometric.utils.loop import add_remaining_self_loops
 from torch_geometric.nn.inits import glorot, zeros
 from torch.nn import Parameter
@@ -18,7 +18,7 @@ class ODEFuncGreed(ODEFunc):
 
   def __init__(self, in_features, out_features, opt, data, device, bias=False):
     super(ODEFuncGreed, self).__init__(opt, data, device)
-
+    assert opt['self_loop_weight'] == 0, 'greed does not work with self-loops as eta becomes zero everywhere'
     if opt['self_loop_weight'] > 0:
       self.edge_index, self.edge_weight = add_remaining_self_loops(data.edge_index, data.edge_attr,
                                                                    fill_value=opt['self_loop_weight'])
@@ -30,10 +30,10 @@ class ODEFuncGreed(ODEFunc):
     self.K = Parameter(torch.Tensor(out_features, 1))
     self.Q = Parameter(torch.Tensor(out_features, 1))
 
-    self.deg_inv_sqrt = self.get_inv_root_degree(data)
+    self.deg_inv_sqrt = self.get_deg_inv_sqrt(data)
     self.deg_inv = self.deg_inv_sqrt * self.deg_inv_sqrt
 
-    self.weight = Parameter(torch.Tensor(in_features, out_features))
+    self.W = Parameter(torch.Tensor(in_features, out_features))
 
     if bias:
       self.bias = Parameter(torch.Tensor(out_features))
@@ -45,13 +45,19 @@ class ODEFuncGreed(ODEFunc):
     self.reset_parameters()
 
   def get_deg_inv_sqrt(self, data):
-    deg = torch_sparse.sum(data.edge_index, dim=1)
+    edge_index = data.edge_index
+    edge_weight = torch.ones((edge_index.size(1),), dtype=data.x.dtype,
+                             device=edge_index.device)
+    row, col = edge_index[0], edge_index[1]
+    deg = scatter_add(edge_weight, col, dim=0, dim_size=self.n_nodes)
     deg_inv_sqrt = deg.pow_(-0.5)
     deg_inv_sqrt = deg_inv_sqrt.masked_fill_(deg_inv_sqrt == float('inf'), 0.)
     return deg_inv_sqrt
 
   def reset_parameters(self):
-    glorot(self.weight)
+    glorot(self.W)
+    glorot(self.K)
+    glorot(self.Q)
     zeros(self.bias)
 
   def symmetrically_normalise(self, M):
@@ -63,57 +69,83 @@ class ODEFuncGreed(ODEFunc):
     M = torch_sparse.mul(M, self.deg_inv_sqrt.view(1, -1))
     return M
 
-  def get_tau(self, x, edge):
-    src = x[edge[0, :]]
-    dst = x[edge[1, :]]
-    tau = torch.tanh(src.t @ self.K + dst.t @ self.Q)
-    # todo check this is the correct transpose
-    tau_transpose = torch.tanh(dst.t @ self.K + src.t @ self.Q)
+  def get_tau(self, x):
+    """
+    Tau plays a role similar to the diffusivity / attention function in BLEND. Here the choice of nonlinearity is not
+    plug and play as everything is defined on the level of the energy and so the derivatives are hardcoded.
+    #todo why do we hardcode the derivatives? Couldn't we just use autograd?
+    @param x:
+    @param edge:
+    @return:
+    """
+    src_x, dst_x = self.get_src_dst(x)
+    tau = torch.tanh(src_x @ self.K + dst_x @ self.Q)
+    tau_transpose = torch.tanh(dst_x @ self.K + src_x @ self.Q)
     return tau, tau_transpose
 
-  def get_gamma(self, x, edge, tau, tau_transpose, epsilon=10e-6):
+  def get_src_dst(self, x):
     """
-    get gamma_ij = |Z_ij|^2
-    get eta(i) = \prod_{j \in N(i)} gamma_{ij}}^{1/d_i}
+    Get the values of a dense n-by-d matrix
+    @param x:
+    @return:
+    """
+    src = x[self.edge_index[0, :]]
+    dst = x[self.edge_index[1, :]]
+    return src, dst
+
+  def get_gamma(self, x, tau, tau_transpose, epsilon=1e-6):
+    """
+
+    @param x:
+    @param edge:
+    @param tau:
+    @param tau_transpose:
+    @param epsilon:
+    @return:
     """
     # todo break up this function
-    src = x[edge[0, :]]
-    dst = x[edge[1, :]]
-    src_degree = self.deg_inv_sqrt[edge[0, :]]
-    dst_degree = self.deg_inv_sqrt[edge[1, :]]
-
-    src_term = torch.div(tau * src, src_degree)
+    src_x, dst_x = self.get_src_dst(x)
+    src_degree, dst_degree = self.get_src_dst(self.deg_inv_sqrt)
+    src_term = torch.div(tau * src_x, src_degree.unsqueeze(dim=1))
     src_term.masked_fill_(src_term == float('inf'), 0.)
-    dst_term = torch.div(tau_transpose * dst, dst_degree)
+    dst_term = torch.div(tau_transpose * dst_x, dst_degree.unsqueeze(dim=1))
     dst_term.masked_fill_(dst_term == float('inf'), 0.)
-    unormalised_gamma = self.W @ (src_term - dst_term)
-    sml_gamma = torch.sum(unormalised_gamma * unormalised_gamma, dim=1)
-    # todo check this
-    eta = torch.pow(torch_scatter.scatter_mul(sml_gamma, edge[1, :]), self.deg_inv)
-    src_eta = eta[edge[0, :]]
-    dst_eta = eta[edge[1, :]]
-    big_gamma = -torch.div(src_eta + dst_eta, 2 * sml_gamma)
-    with torch.no_grad:
-      mask = sml_gamma < epsilon
-    big_gamma[mask] = 0
+    temp = (src_term - dst_term) @ self.W
+    edge_weight = torch.sum(temp * temp, dim=1)
+    # The degree is the thing that provides a notion of dimension on a graph
+    # eta is the determinant of the ego graph. This should slow down diffusion where the grad is large
+    eta = torch.pow(scatter_mul(edge_weight, self.edge_index[1, :]), self.deg_inv)
+    src_eta, dst_eta = self.get_src_dst(eta)
+    big_gamma = -torch.div(src_eta + dst_eta, 2 * edge_weight)
+    with torch.no_grad():
+      mask = edge_weight < epsilon
+      big_gamma[mask] = 0
     return big_gamma
 
   def get_dynamics(self, f, gamma, tau, Ws):
-    tau_square = tau * tau
-    tau2 = tau_square @ f
-    tau3 = tau * tau_square @ f
-    T0 = gamma * tau2
+    """
+    These dynamics follow directly from differentiating the energy
+    The structure of T0,T1 etc. is a direct result of differentiating the hyperbolic tangent nonlinearity.
+    Note: changing the nonlinearity will change the equations
+    Returns: L - a generalisation of the Laplacian, but using attention
+             R1, R2 are a result of tau being a function of the features and both are zero if tau is not dependent on
+             features
+    """
     # todo tau is probably sparse so this won't work
+    tau2 = tau * tau
+    tau3 = tau * tau2
+    T0 = gamma * tau2
     T1 = gamma * tau * tau.T
     T2 = gamma * (tau - tau3)
     T3 = gamma * (tau.T - tau.T * tau2)
+    # the next line has the spirit of a Laplacian e.g. D-A
     T4 = self.symmetrically_normalise(torch.diag(torch.sum(T2, dim=1)) - T3)
     T5 = self.symmetrically_normalise(T3.T)
     L = self.symmetrically_normalise(torch.diag(torch.sum(T0, dim=1)) - T1)
     fWs = torch.matmul(f, Ws)
     R1 = torch.sum(f * (T4 @ fWs), dim=1)
     # todo check that this is the same as what Francesco wrote
-    R2 = T2.T @ torch.sum(self.symmetrically_normalise(f @ fWs), dim=1) - torch.sum(f * T5 @ fWs)
+    R2 = T2.T @ self.deg_inv * torch.sum(f * fWs, dim=1) - torch.sum(f * T5 @ fWs, dim=1)
     return L, R1, R2
 
   def forward(self, t, x):  # t is needed when called by the integrator
@@ -121,12 +153,14 @@ class ODEFuncGreed(ODEFunc):
       raise MaxNFEException
 
     self.nfe += 1
-    gamma, tau = self.multihead_att_layer(x, self.edge_index)
-    Ws = self.weight.T @ self.weight
+    Ws = self.W.T @ self.W
+    tau, tau_transpose = self.get_tau(x)
+    gamma = self.get_gamma(x, tau, tau_transpose, epsilon=1e-6)
     L, R1, R2 = self.get_dynamics(x, gamma, tau, Ws)
     f = L @ x
     f = torch.matmul(f, self.Ws)
     f = f + R1 @ self.K.T + R2 @ self.Q.T
+    # todo check x0 is the encoded value of features + pos_enc
     f = f - self.mu(f - self.x0)
     return f
 
--- test/test_function_greed.py
+++ test/test_function_greed.py
@@ -2,19 +2,27 @@
 # -*- coding: utf-8 -*-
 """
 Test GREED attention
+Note: Greed only works WITHOUT self-loops
 """
 import unittest
 import torch
 from torch import tensor
 from torch import nn
 import torch_sparse
-from function_greed import SpGraphGreedAttentionLayer, ODEFuncGreed
+from function_greed import ODEFuncGreed
 from torch_geometric.utils import softmax, to_dense_adj
 from data import get_dataset
 from test_params import OPT
 
 
-class GreedAttentionTests(unittest.TestCase):
+class Data:
+  def __init__(self, edge_index, x):
+    self.x = x
+    self.edge_index = edge_index
+    self.edge_attr = None
+
+
+class GreedTests(unittest.TestCase):
   def setUp(self):
     self.edge = tensor([[0, 2, 2, 1], [1, 0, 1, 2]])
     self.x = tensor([[1., 2.], [3., 2.], [4., 5.]], dtype=torch.float)
@@ -24,14 +32,39 @@ class GreedAttentionTests(unittest.TestCase):
     self.W = tensor([[2, 1], [3, 2]], dtype=torch.float)
     self.alpha = tensor([[1, 2, 3, 4]], dtype=torch.float)
     self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    opt = {'dataset': 'Citeseer', 'self_loop_weight': 1, 'leaky_relu_slope': 0.2, 'beta_dim': 'vc', 'heads': 2,
-                'K': 10,
-                'attention_norm_idx': 0, 'add_source': False, 'alpha': 1, 'alpha_dim': 'vc', 'beta_dim': 'vc',
-                'hidden_dim': 6, 'linear_attention': True, 'augment': False, 'adjoint': False,
-                'tol_scale': 1, 'time': 1, 'ode': 'ode', 'input_dropout': 0.5, 'dropout': 0.5, 'method': 'euler',
-                'mixed_block': True, 'max_nfe': 1000, 'mix_features': False, 'attention_dim': 32, 'rewiring': None,
-                'no_alpha_sigmoid': False, 'reweight_attention': False, 'kinetic_energy': None, 'jacobian_norm2': None, 'total_deriv': None, 'directional_penalty': None, 'beltrami': False}
+    opt = {'dataset': 'Citeseer', 'self_loop_weight': 0, 'leaky_relu_slope': 0.2, 'beta_dim': 'vc', 'heads': 2,
+           'K': 10,
+           'attention_norm_idx': 0, 'add_source': False, 'alpha': 1, 'alpha_dim': 'vc', 'beta_dim': 'vc',
+           'hidden_dim': 6, 'linear_attention': True, 'augment': False, 'adjoint': False,
+           'tol_scale': 1, 'time': 1, 'ode': 'ode', 'input_dropout': 0.5, 'dropout': 0.5, 'method': 'euler',
+           'mixed_block': True, 'max_nfe': 1000, 'mix_features': False, 'attention_dim': 32, 'rewiring': None,
+           'no_alpha_sigmoid': False, 'reweight_attention': False, 'kinetic_energy': None, 'jacobian_norm2': None,
+           'total_deriv': None, 'directional_penalty': None, 'beltrami': True}
     self.opt = {**OPT, **opt}
+    self.data = Data(self.edge, self.x)
+    self.greed_func = ODEFuncGreed(2, 2, self.opt, self.data, torch.device('cpu'))
+
   def tearDown(self) -> None:
     pass
 
+  def test_get_deg_inv_sqrt(self):
+    pass
+
+  def test_symmetrically_normalise(self):
+    pass
+
+  def test_get_tau(self):
+    tau, tau_transpose = self.greed_func.get_tau(self.x)
+    self.assertTrue(tau.shape == torch.Size((self.edge.shape[1], 1)))
+    self.assertTrue(tau.shape == tau_transpose.shape)
+    # check that tau(2,1) is  tau_transpose(1,2) and vice versa
+    self.assertTrue(tau[2] == tau_transpose[3])
+    self.assertTrue(tau[3] == tau_transpose[2])
+
+  def test_get_gamma(self):
+    tau, tau_transpose = self.greed_func.get_tau(self.x)
+    gamma = self.greed_func.get_gamma(self.x, tau, tau_transpose, epsilon=1e-6)
+    self.assertTrue(list(gamma.shape) == [self.edge.shape[1]])
+
+  def test_get_dynamics(self):
+    pass
