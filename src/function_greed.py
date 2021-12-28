fixed overflow in get_gamma

src/function_greed.py
@@ -13,6 +13,13 @@ from torch.nn import Parameter
 from utils import MaxNFEException
 from base_classes import ODEFunc
 
+# REMOVE SOURCES OF RANDOMNESS
+import numpy as np
+import random
+torch.manual_seed(0)
+random.seed(0)
+np.random.seed(0)
+# torch.use_deterministic_algorithms(True)
 
 class ODEFuncGreed(ODEFunc):
 
@@ -60,13 +67,15 @@ class ODEFuncGreed(ODEFunc):
     edge_weight = torch.ones((edge_index.size(1),), dtype=data.x.dtype,
                              device=edge_index.device)
     row, col = edge_index[0], edge_index[1]
-    deg = scatter_add(edge_weight, col, dim=0, dim_size=self.n_nodes)
+    deg = scatter_add(edge_weight, row, dim=0, dim_size=self.n_nodes)
     deg_inv_sqrt = deg.pow_(-0.5)
     deg_inv_sqrt = deg_inv_sqrt.masked_fill_(deg_inv_sqrt == float('inf'), 0.)
     return deg_inv_sqrt
 
   def symmetrically_normalise(self, x, edge):
     """
+    symmetrically normalise a sparse matrix with values x and edges edge. Note that edge need not be the edge list
+    of the input graph as we could be normalising a Laplacian, which additionally has values on the diagonal
     D^{-1/2}MD^{-1/2}
     where D is the degree matrix
     """
@@ -125,7 +134,13 @@ class ODEFuncGreed(ODEFunc):
   def get_gamma(self, metric, epsilon=1e-6):
     # The degree is the thing that provides a notion of dimension on a graph
     # eta is the determinant of the ego graph. This should slow down diffusion where the grad is large
-    eta = torch.pow(scatter_mul(metric, self.edge_index[1, :]), self.deg_inv)
+    row, col = self.edge_index
+    # power = self.deg_inv[row]
+    # metric = torch.pow(metric, power)
+    # eta = scatter_mul(metric, row)
+    # eta = torch.pow(scatter_mul(metric, self.edge_index[1, :]), self.deg_inv)
+    log_eta = self.deg_inv * scatter_add(torch.log(metric), row)
+    eta = torch.exp(log_eta)
     src_eta, dst_eta = self.get_src_dst(eta)
     gamma = -torch.div(src_eta + dst_eta, 2 * metric)
     with torch.no_grad():
@@ -167,7 +182,7 @@ class ODEFuncGreed(ODEFunc):
     @param D: Matrix that is row summed to play the role of the degree matrix
     @return: A Laplacian form
     """
-    degree = scatter_add(D, self.edge_index[1, :], dim=-1, dim_size=self.n_nodes)
+    degree = scatter_add(D, self.edge_index[0, :], dim=-1, dim_size=self.n_nodes)
     edges = torch.cat([self.edge_index, self.self_loops], dim=1)
     values = torch.cat([-A, degree], dim=-1)
     L = self.symmetrically_normalise(values, edges)

test/test_function_greed.py
@@ -13,6 +13,8 @@ from function_greed import ODEFuncGreed
 from torch_geometric.utils import softmax, to_dense_adj
 from data import get_dataset
 from test_params import OPT
+import numpy as np
+from torch_scatter import scatter_add, scatter_mul
 
 
 class Data:
@@ -26,7 +28,7 @@ class GreedTests(unittest.TestCase):
   def setUp(self):
     self.edge = tensor([[0, 2, 2, 1, 1, 3, 2, 3], [2, 0, 1, 2, 3, 1, 3, 2]])
     self.x = tensor([[1., 2.], [3., 2.], [4., 5.], [6, 7]], dtype=torch.float)
-    self.W = tensor([[2, 1], [3, 2]], dtype=torch.float)
+    self.W = tensor([[0.2, 0.1], [0.3, 0.2]], dtype=torch.float)
     self.alpha = tensor([[1, 2, 3, 4]], dtype=torch.float)
     self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     opt = {'dataset': 'Citeseer', 'self_loop_weight': 0, 'leaky_relu_slope': 0.2, 'beta_dim': 'vc', 'heads': 2,
@@ -44,11 +46,62 @@ class GreedTests(unittest.TestCase):
   def tearDown(self) -> None:
     pass
 
+  def test_spvm(self):
+    ei = self.greed_func.edge_index
+    values = torch.rand((ei.shape[1]))
+    vector = torch.rand((self.greed_func.n_nodes))
+    dense_mat = to_dense_adj(ei, edge_attr=values).squeeze()
+    out = self.greed_func.spvm(ei, values, vector)
+    dense_out = dense_mat @ vector
+    self.assertTrue(torch.all(dense_out == out))
+
+  def test_get_R1(self):
+    x = torch.rand((self.greed_func.n_nodes, 2))
+    tau, tau_transpose = self.greed_func.get_tau(x)
+    metric = self.greed_func.get_metric(x, tau, tau_transpose)
+    gamma = self.greed_func.get_gamma(metric)
+    tau = tau.flatten()
+    tau_transpose = tau_transpose.flatten()
+    tau2 = tau * tau
+    tau3 = tau * tau2
+    T2 = gamma * (tau - tau3)
+    T3 = gamma * (tau_transpose - tau_transpose * tau2)
+    Ws = self.W @ self.W.t()
+    fWs = torch.matmul(x, Ws)
+    R1 = self.greed_func.get_R1(x, T2, T3, fWs)
+    self.assertTrue(R1.shape[0] == self.greed_func.n_nodes)
+
+  def test_get_R2(self):
+    x = torch.rand((self.greed_func.n_nodes, 2))
+    tau, tau_transpose = self.greed_func.get_tau(x)
+    metric = self.greed_func.get_metric(x, tau, tau_transpose)
+    gamma = self.greed_func.get_gamma(metric)
+    tau = tau.flatten()
+    tau_transpose = tau_transpose.flatten()
+    tau2 = tau * tau
+    tau3 = tau * tau2
+    T2 = gamma * (tau - tau3)
+    T3 = gamma * (tau_transpose - tau_transpose * tau2)
+    Ws = self.W @ self.W.t()
+    fWs = torch.matmul(x, Ws)
+    nn = self.greed_func.n_nodes
+    transposed_edge_index, T3_transpose = torch_sparse.transpose(self.greed_func.edge_index, T3, nn, nn)
+    T5 = self.greed_func.symmetrically_normalise(T3_transpose, transposed_edge_index)
+    R2 = self.greed_func.get_R2(T2, T5, transposed_edge_index, x, fWs)
+    self.assertTrue(R2.shape[0] == self.greed_func.n_nodes)
+
   def test_get_deg_inv_sqrt(self):
-    pass
+    deg_inv_sqrt = self.greed_func.get_deg_inv_sqrt(self.data)
+    self.assertTrue(deg_inv_sqrt.shape[0] == self.data.x.shape[0])
+    self.assertTrue(
+      torch.all(torch.isclose(deg_inv_sqrt, torch.tensor(np.sqrt([1, 1 / 2, 1 / 3, 1 / 2]), dtype=deg_inv_sqrt.dtype)))
+    )
 
   def test_symmetrically_normalise(self):
-    pass
+    values = torch.rand((self.greed_func.edge_index.shape[1]))
+    normed_x = self.greed_func.symmetrically_normalise(values, self.greed_func.edge_index)
+    self.assertTrue(normed_x.shape == values.shape)
+    self.assertTrue(torch.all(normed_x >= 0))
 
   def test_get_tau(self):
     tau, tau_transpose = self.greed_func.get_tau(self.x)
@@ -72,6 +125,16 @@ class GreedTests(unittest.TestCase):
     self.assertTrue(torch.all(gamma < 0))
     dense_gamma = to_dense_adj(self.greed_func.edge_index, edge_attr=gamma).squeeze()
     self.assertTrue(torch.all(dense_gamma == dense_gamma.t()))
+    row, col = self.greed_func.edge_index
+    power = self.greed_func.deg_inv[row]
+    metric_power = torch.pow(metric, power)
+    eta1 = scatter_mul(metric_power, row)
+    eta2 = torch.pow(scatter_mul(metric, row), self.greed_func.deg_inv)
+    log_eta = self.greed_func.deg_inv * scatter_add(torch.log(metric), row)
+    eta3 = torch.exp(log_eta)
+    self.assertTrue(torch.all(eta1 == eta2))
+    self.assertTrue(torch.all(torch.isclose(eta1, eta3)))
+
     # currently an assertion prevents this running
     # self.opt['self_loop_weight'] = 1
     # greed_func = ODEFuncGreed(2, 2, self.opt, self.data, torch.device('cpu'))
@@ -90,17 +153,36 @@ class GreedTests(unittest.TestCase):
     # the Laplacian has one value for each edge plus one value for each self edge
     self.assertTrue(L.shape[0] == self.greed_func.n_nodes + self.greed_func.edge_index.shape[1])
 
-  def test_get_R1(self):
-    pass
-
-  def test_get_R2(self):
-    pass
-
   def test_get_laplacian_form(self):
-    pass
+    n_edges = self.greed_func.edge_index.shape[1]
+    n_nodes = self.greed_func.n_nodes
+    A = torch.rand((n_edges))
+    D = torch.rand((n_edges))
+    L = self.greed_func.get_laplacian_form(A, D)
+    self.assertTrue(L.shape[0] == n_edges + n_nodes)
+    self.assertTrue(torch.all(L[0:n_edges] <= 0))
+    self.assertTrue(torch.all(L[n_edges:] >= 0))
+    dense_A = to_dense_adj(self.greed_func.edge_index, edge_attr=A).squeeze()
+    dense_D = to_dense_adj(self.greed_func.edge_index, edge_attr=D).squeeze()
+    degree = torch.diag(dense_D.sum(dim=1))
+    L_unorm = degree - dense_A
+    normaliser = torch.diag(self.greed_func.deg_inv_sqrt)
+    L_dense = normaliser @ L_unorm @ normaliser
+    edges = torch.cat([self.greed_func.edge_index, self.greed_func.self_loops], dim=1)
+    L_dense1 = to_dense_adj(edges, edge_attr=L).squeeze()
+    self.assertTrue(torch.all(L_dense == L_dense1))
 
-  def test_spvm(self):
-    pass
+  def test_scatter_add(self):
+    ei = torch.tensor([[0, 0, 1, 1, 2, 2], [1, 2, 0, 2, 0, 1]])
+    x = torch.tensor([1, 2, 3, 4, 5, 6])
+    degree = scatter_add(x, ei[0, :], dim=-1, dim_size=3)
+    self.assertTrue(torch.all(degree == torch.tensor([3, 7, 11])))
+
+  def test_scatter_mul(self):
+    ei = torch.tensor([[0, 0, 1, 1, 2, 2], [1, 2, 0, 2, 0, 1]])
+    x = torch.tensor([1, 2, 3, 4, 5, 6])
+    degree = scatter_mul(x, ei[0, :], dim=-1, dim_size=3)
+    self.assertTrue(torch.all(degree == torch.tensor([2, 12, 30])))
 
   def test_greed(self):
     self.greed_func.x0 = self.x