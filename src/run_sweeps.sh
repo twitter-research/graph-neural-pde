73ff177c602e75a8ede736d47b64366bd1a94e64
James Rowbottom <jr1419@ic.ac.uk>
2022-01-09 16:18:29 +0000

    run sweep fix

--- src/run_sweeps.sh
+++ src/run_sweeps.sh
@@ -2,5 +2,5 @@
 
 for i in {0..7}
 do
-    CUDA_VISIBLE_DEVICES=$(($i % 8)) wandb agent graph-neural-diffusion/graph-neural-pde-src/$1 &
+    CUDA_VISIBLE_DEVICES=$(($i % 8)) wandb agent jrowbottomwnb/graph-neural-pde-src/$1 &
 done
--- src/run_sweeps_jr.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-for i in {0..7}
-do
-    CUDA_VISIBLE_DEVICES=$(($i % 8)) wandb agent jrowbottomwnb/graph-neural-pde-src/$1 &
-done
