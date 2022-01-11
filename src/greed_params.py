a312508b50ad70d49f9dd476c570cae448da3db6
James Rowbottom <jr1419@ic.ac.uk>
2022-01-11 21:59:29 +0000

    removing early stop from tune as doesn't work with Euler

--- src/greed_params.py
+++ src/greed_params.py
@@ -88,7 +88,7 @@ def t_or_f(tf_str):
 
 def tf_ablation_args(opt):
     for arg in ['test_no_chanel_mix','test_omit_metric','test_mu_0','test_tau_remove_tanh','test_tau_symmetric',
-                'use_mlp', 'add_source', 'no_early']:
+                'use_mlp', 'add_source']:
         str_tf = opt[arg]
         bool_tf = t_or_f(str)
         opt[arg] = bool_tf
