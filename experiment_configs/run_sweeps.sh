e091c8bdad28b308703da342cc850350e77f7c54
Ben Chamberlain <bchamberlain@twitter.com>
2022-01-20 12:07:31 +0000

    path in run_sweeps

--- experiment_configs/run_sweeps.sh
+++ experiment_configs/run_sweeps.sh
@@ -2,5 +2,5 @@
 
 for i in {0..7}
 do
-    CUDA_VISIBLE_DEVICES=$(($i % 8)) wandb agent bchamberlain/graph-neural-pde/$1 &
+    CUDA_VISIBLE_DEVICES=$(($i % 8)) wandb agent bchamberlain/graph-neural-pde-src/$1 &
 done
