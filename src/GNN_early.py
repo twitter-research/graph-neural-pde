benchmark - consistently hit's 85% for function_greed_linear_homo.py with the simplifications with T0 and T1, and n softmax symmetric attention. One big fix is to removing initialisation on W when it was meant to be identity. This would have been a long term bug

src/GNN_early.py
@@ -78,7 +78,7 @@ class GNNEarly(BaseGNN):
     with torch.no_grad():
       self.set_solver_m2()
 
-    if self.opt['function'] in ['greed_linear', 'greed_linear_homo', 'laplacian_greed']:
+    if self.opt['function'] in ['greed_linear', 'greed_linear_homo']:
       self.odeblock.odefunc.set_x_0(x) #this x is actually z
       self.odeblock.odefunc.set_tau_0()
       if self.opt['test_linear_L0']:

src/function_greed_linear_homo.py
@@ -70,12 +70,13 @@ class ODEFuncGreedLinH(ODEFuncGreed):
         glorot(self.Qp)
       glorot(self.Kx)
       glorot(self.Kp)
+      glorot(self.W) #for non beltrami done in function_greed
     else:
       if not self.opt['test_tau_symmetric']:
         glorot(self.Q)
       glorot(self.K)
     zeros(self.bias)
-    glorot(self.W)
+
 
 
   def set_x_0(self, x_0):
@@ -159,9 +160,11 @@ class ODEFuncGreedLinH(ODEFuncGreed):
     @param D: Matrix that is row summed to play the role of the degree matrix
     @return: A Laplacian form
     """
-    degree = scatter_add(D, self.edge_index[0, :], dim=-1, dim_size=self.n_nodes)
+    degree = scatter_add(D, self.edge_index[0, :], dim=0, dim_size=self.n_nodes)
     if self.opt['T0term_normalisation']: #like identity in A_hat = A - I
       degree = sym_row_col(self.self_loops, degree, self.n_nodes)
+      degree = torch.ones(degree.shape) #set this to ones to replicate good result
+      # degree = self.symmetrically_normalise(degree, self.self_loops)
 
     if self.opt['T1term_normalisation']: #like A in A_hat = A - I
       A = sym_row_col(self.edge_index, A, self.n_nodes)