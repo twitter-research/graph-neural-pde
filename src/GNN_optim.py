a98ced7012d67e101b6022f0f19f0a3b5dcd41d1
JRowbottomGit <jabrowbottom@gmail.com>
2022-02-14 20:55:57 +0000

    grand_increments_9.yaml for PLanetoid run on function_greed_linear_homo

---
+++ src/GNN_optim.py
@@ -0,0 +1,78 @@
+import torch
+from torch import nn
+import torch.nn.functional as F
+from base_classes import BaseGNN
+from model_configurations import set_block, set_function
+
+
+# Define the GNN model.
+class GNN(BaseGNN):
+  def __init__(self, opt, dataset, device=torch.device('cpu')):
+    super(GNN, self).__init__(opt, dataset, device)
+    self.f = set_function(opt)
+    block = set_block(opt)
+    time_tensor = torch.tensor([0, self.T]).to(device)
+    self.odeblock = block(self.f, self.regularization_fns, opt, dataset.data, device, t=time_tensor).to(device)
+
+  def forward(self, x, pos_encoding=None):
+    # Encode each node based on its feature.
+    if self.opt['use_labels']:
+      y = x[:, -self.num_classes:]
+      x = x[:, :-self.num_classes]
+
+    if self.opt['beltrami']:
+      x = F.dropout(x, self.opt['input_dropout'], training=self.training)
+      x = self.mx(x)
+      p = F.dropout(pos_encoding, self.opt['input_dropout'], training=self.training)
+      p = self.mp(p)
+      x = torch.cat([x, p], dim=1)
+    else:
+      x = F.dropout(x, self.opt['input_dropout'], training=self.training)
+      x = self.m1(x)
+
+    if self.opt['use_mlp']:
+      x = F.dropout(x, self.opt['dropout'], training=self.training)
+      x = F.dropout(x + self.m11(F.relu(x)), self.opt['dropout'], training=self.training)
+      x = F.dropout(x + self.m12(F.relu(x)), self.opt['dropout'], training=self.training)
+    # todo investigate if some input non-linearity solves the problem with smooth deformations identified in the ANODE paper
+
+    if self.opt['use_labels']:
+      x = torch.cat([x, y], dim=-1)
+
+    if self.opt['batch_norm']:
+      x = self.bn_in(x)
+
+    # Solve the initial value problem of the ODE.
+    if self.opt['augment']:
+      c_aux = torch.zeros(x.shape).to(self.device)
+      x = torch.cat([x, c_aux], dim=1)
+
+    self.odeblock.set_x0(x)
+
+    if self.opt['function'] in ['greed_linear', 'greed_linear_homo']:
+      self.odeblock.odefunc.set_x_0(x) #this x is actually z
+      self.odeblock.odefunc.set_tau_0()
+      if self.opt['test_linear_L0']:
+        self.odeblock.odefunc.set_L0()
+
+    if self.training and self.odeblock.nreg > 0:
+      z, self.reg_states = self.odeblock(x)
+    else:
+      z = self.odeblock(x)
+
+    if self.opt['augment']:
+      z = torch.split(z, x.shape[1] // 2, dim=1)[0]
+
+    # Activation.
+    z = F.relu(z)
+
+    if self.opt['fc_out']:
+      z = self.fc(z)
+      z = F.relu(z)
+
+    # Dropout.
+    z = F.dropout(z, self.opt['dropout'], training=self.training)
+
+    # Decode each node embedding to get node label.
+    z = self.m2(z)
+    return z
