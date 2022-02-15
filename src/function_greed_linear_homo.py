f23f2e72ab866846c51e0794a1e6eb7414797ff9
JRowbottomGit <jabrowbottom@gmail.com>
2022-02-15 15:39:51 +0000

    add measure back to signature

--- src/function_greed_linear_homo.py
+++ src/function_greed_linear_homo.py
@@ -184,7 +184,7 @@ class ODEFuncGreedLinH(ODEFuncGreed):
     elif self.opt['laplacian_norm'] == "lap_symmAttM_RowSumnorm":
       # A = torch.cat([A, torch.zeros(degree.shape, device=A.device)], dim=-1)
       # L = sym_row_col_att_measure(edges, A, values, self.measure, self.n_nodes)
-      L = sym_row_col_att_measure(self.edge_index, A, edges, values, self.n_nodes)
+      L = sym_row_col_att_measure(self.edge_index, A, edges, values, self.measure, self.n_nodes)
     elif self.opt['laplacian_norm'] == "lap_noNorm":
       L = values
 
