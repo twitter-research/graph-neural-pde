93567e48bb035f0ae8081439d557750621e2411f
Ubuntu <ubuntu@ip-172-31-18-31.ec2.internal>
2022-05-04 18:56:25 +0000

    Added cora config

--- src/run_sweeps.sh
+++ src/run_sweeps.sh
@@ -2,5 +2,5 @@
 
 for i in {0..7}
 do
-    CUDA_VISIBLE_DEVICES=$(($i % 8)) wandb agent graph_neural_diffusion/greed_tuning_2/$1 &
+    CUDA_VISIBLE_DEVICES=$(($i % 8)) wandb agent graph_neural_diffusion/final_cora_tune_2hops/$1 &
 done
