80f1b1c76faf7b1c6fde599e8fb35461edab556d
JRowbottomGit <jabrowbottom@gmail.com>
2022-05-08 18:36:34 +0100

    final2_small_hetero_tune.yaml

--- src/greed_plots.py
+++ src/greed_plots.py
@@ -4,21 +4,19 @@ import seaborn as sns
 import matplotlib. pyplot as plt
 
 sns.set_theme(style="whitegrid")
-
 rs = np.random.RandomState(365)
 values = rs.randn(365, 4).cumsum(axis=0)
 dates = pd.date_range("1 1 2016", periods=365, freq="D")
 data = pd.DataFrame(values, dates, columns=["A", "B", "C", "D"])
 data = data.rolling(7).mean()
-
 sns.lineplot(data=data, palette="tab10", linewidth=2.5)
 # plt.show()
-
 
 def sinplot(flip=1):
     x = np.linspace(0, 14, 100)
     for i in range(1, 7):
         plt.plot(x, np.sin(x + i * .5) * (7 - i) * flip)
+
 f = plt.figure(figsize=(6, 6))
 gs = f.add_gridspec(2, 2)
 with sns.axes_style("darkgrid"):
@@ -43,21 +41,51 @@ fig, ax = plt.subplots(figsize=(16,10), dpi= 80)
 sns.stripplot(df.cty, df.hwy, jitter=0.25, size=8, ax=ax, linewidth=.5)
 # Decorations
 plt.title('Use jittered plots to avoid overlapping of points', fontsize=22)
-# plt.show()
-
-
-df = pd.read_csv("~/Downloads/wandb_export_2022-05-07T14_06_35.099+01_00.csv")
-piv = pd.pivot_table(df, values="test_mean", index="target_homoph", columns="gnl_W_style", aggfunc=np.max)
-fig, ax = plt.subplots()
-sns.lineplot(data=piv, palette="tab10", linewidth=2.5)
 fig.show()
 
-print(df)
-for target_homoph in ['0.00', '0.10', '0.20', '0.30', '0.40', '0.50', '0.60', '0.70', '0.80', '0.90', '1.00']:
-    for w_type in ["diag_dom", "neg_prod", "prod", "sum"]:
-        new_cols = ["target_homoph", "gnl_W_style", "time", "step_size", "test_mean"]
-        newdf = df[new_cols][(df.target_homoph == float(target_homoph)) & (df.gnl_W_style == w_type)]
-        max_idx = newdf[['test_mean']].idxmax()
-        max_time = df.iloc[max_idx]['time'] #.values
-        max_step = df.iloc[max_idx]['step_size'] #.values
+def syn_cora_plot(path):
+    df = pd.read_csv(path)
+    gnl_df = df[(df.function == "greed_non_linear")]
+    gnl_piv = pd.pivot_table(gnl_df, values="test_mean", index="target_homoph", columns="gnl_W_style", aggfunc=np.max)
+    base_df = df[(df.function != "greed_non_linear")]
+    base_piv = pd.pivot_table(base_df, values="test_mean", index="target_homoph", columns="function", aggfunc=np.max)
+    piv = pd.merge(gnl_piv, base_piv, on=['target_homoph'])
+    fig, ax = plt.subplots()
+    sns.lineplot(data=piv, palette="tab10", linewidth=2.5)
+    fig.show()
 
+def syn_cora_energy(path):
+    df = pd.read_csv(path)
+    gnl_df = df[(df.function == "greed_non_linear")]
+    # gnl_piv = pd.pivot_table(gnl_df,
+    #                          values=["T0_dirichlet_mean", "T0r_dirichlet_mean",
+    #                                  "TN_dirichlet_mean", "TNr_dirichlet_mean"],
+    #                          index="target_homoph", columns=["gnl_W_style"], aggfunc=np.max)
+    gnl_df['W_homoph'] = df.apply(lambda row: str(row.gnl_W_style) + "_" + str(row.target_homoph), axis=1)
+    gnl_piv = pd.pivot_table(gnl_df,
+                             values=["T0_dirichlet_mean", "T0r_dirichlet_mean",
+                                     "TN_dirichlet_mean", "TNr_dirichlet_mean"],
+                             index="W_homoph", aggfunc=np.max)
+
+    fig, ax = plt.subplots()
+    tips = sns.load_dataset("tips")
+    sns.catplot(x="day", y="total_bill", data=tips)
+    # sns.catplot(x="W_homoph", y="value", jitter=False, data=gnl_piv)
+    fig.show()
+
+def syn_cora_best_times(path):
+    df = pd.read_csv(path)
+    print(df)
+    for target_homoph in ['0.00', '0.10', '0.20', '0.30', '0.40', '0.50', '0.60', '0.70', '0.80', '0.90', '1.00']:
+        for w_type in ["diag_dom", "neg_prod", "prod", "sum"]:
+            new_cols = ["target_homoph", "gnl_W_style", "time", "step_size", "test_mean"]
+            newdf = df[new_cols][(df.target_homoph == float(target_homoph)) & (df.gnl_W_style == w_type)]
+            max_idx = newdf[['test_mean']].idxmax()
+            max_time = df.iloc[max_idx]['time'] #.values
+            max_step = df.iloc[max_idx]['step_size'] #.values
+
+if __name__ == "__main__":
+    path = "../ablations/ablation_syn_cora.csv"
+    # syn_cora_plot(path)
+    syn_cora_energy(path)
+    # syn_cora_best_times(path)
