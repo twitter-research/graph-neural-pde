deff0e514420551eb1247e5b0326051babfe1df1
JRowbottomGit <jabrowbottom@gmail.com>
2022-05-10 11:21:44 +0100

    ablation_syn_cora_gcn.yaml - two failed/crashed mopups

--- src/greed_fig1.py
+++ src/greed_fig1.py
@@ -1,7 +1,9 @@
 import sys
 import numpy as np
-import matplotlib.pyplot as plt
+import matplotlib as mpl
 from matplotlib.patches import Circle
+from matplotlib import pyplot as plt
+from pylab import rcParams
 import torch
 import networkx as nx
 from torch_geometric.utils.convert import to_networkx
@@ -14,7 +16,7 @@ def E(q, r0, x, y):
     den = np.hypot(x-r0[0], y-r0[1])**3
     return q * (x - r0[0]) / den, q * (y - r0[1]) / den
 
-def plot_electric():
+def plot_electric(fig=None, ax=None, ax_idx=None, plot=False, save=False):
     # Grid of x, y points
     nx, ny = 64, 64
     x = np.linspace(-2, 2, nx)
@@ -38,8 +40,11 @@ def plot_electric():
         Ex += ex
         Ey += ey
 
-    fig = plt.figure()
-    ax = fig.add_subplot(111)
+    # fig = plt.figure()
+    # ax = fig.add_subplot(111)
+    if ax is None:
+        fig, ax = plt.subplots()
+        ax_idx = 0
 
     # Plot the streamlines with an appropriate colormap and arrow style
     color = 2 * np.log(np.hypot(Ex, Ey))
@@ -51,30 +56,48 @@ def plot_electric():
     for q, pos in charges:
         ax.add_artist(Circle(pos, 0.05, color=charge_colors[q>0]))
 
+    # add graph
+    fig, ax = plot_greed(fig, ax)
+
     ax.set_xlabel('$x$')
     ax.set_ylabel('$y$')
     ax.set_xlim(-2,2)
     ax.set_ylim(-2,2)
     ax.set_aspect('equal')
-    plt.savefig('../ablations/figure1.pdf')
-    plt.show()
+    if save:
+        # plt.savefig('../ablations/figure1.pdf')
+        plt.savefig('../ablations/figure1_a.pdf')
+    if plot:
+        fig.show()
+    return fig, ax
 
-def plot_greed():
-    edge_index = torch.tensor([[0, 1, 1, 2],
-                               [1, 0, 2, 1]], dtype=torch.long)
-    x = torch.tensor([[-1], [0], [1]], dtype=torch.float)
+def plot_greed(fig=None, ax=None, ax_idx=None, plot=False, save=False):
+    edge_index = torch.tensor([[0, 0, 0, 0, 1, 2, 3, 4, 1, 2, 3, 4],
+                               [1, 2, 3, 4, 0, 0, 0, 0, 2, 1, 4, 3]], dtype=torch.long)
+    x = torch.tensor([[0, 0], [-1, -0.5], [-1, 0.5], [1, 0.5], [1, -0.5]], dtype=torch.float)
 
-    pos = torch.tensor([[-1, -1], [0, 1], [1, 0]])
-    pos_i_dict = {i: pos_i[i, :].tolist() for i in range(pos_0.shape[0])}
+    color_vals = [x[i,0] for i in range(x.shape[0])]
+    low, *_, high = sorted(color_vals)
+    norm = mpl.colors.Normalize(vmin=low, vmax=high, clip=True)
+    mapper = mpl.cm.ScalarMappable(norm=norm, cmap=mpl.cm.coolwarm)
 
+    pos = x
+    pos_i_dict = {i: [pos[i,0],pos[i,1]] for i in range(pos.shape[0])}
     graph = Data(x=x, edge_index=edge_index, pos=pos)
-    # Data(edge_index=[2, 4], x=[3, 1])
 
     # draw initial graph
     NXgraph = to_networkx(graph)
-    fig, ax = plt.subplots()
-    nx.draw(NXgraph, pos=pos_i_dict, ax=ax, node_size=params['node_size'],
-          node_color=x_i, cmap=plt.get_cmap('Spectral'), arrows=False, width=0.25) #=params['edge_with'] )
+    if ax is None:
+        fig, ax = plt.subplots()
+        ax_idx = 0
+
+    nx.draw(NXgraph, pos=pos_i_dict, ax=ax, node_size=200, #node_color=color,
+            # cmap=plt.get_cmap('Spectral'),
+            node_color=[mapper.to_rgba(i)
+                        for i in color_vals],
+            # with_labels=True,
+            # font_color='white',
+            arrows=False, width=0.25)
 
     limits = plt.axis('on')
     ax.patch.set_edgecolor('black')
@@ -83,8 +106,12 @@ def plot_greed():
     ax.set_xticks([])
     ax.set_yticks([])
     # plt.title(f"Figure 1", fontsize=16)
-    fig.show()
+    if save:
+        plt.savefig('../ablations/fig1_graph.pdf')
+    if plot:
+        fig.show()
+    return fig, ax
 
 if __name__ == "__main__":
-    plot_electric()
-    plot_greed()
+    plot_electric(plot=True, save=True)
+    # plot_greed(plot=True)
