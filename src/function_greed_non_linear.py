c5f94ca068af4a4cbaf4be22539d83c9931a1b25
JRowbottomGit <jabrowbottom@gmail.com>
2022-08-12 10:27:46 +0100

    fix for time dep W diag

--- src/function_greed_non_linear.py
+++ src/function_greed_non_linear.py
@@ -586,12 +586,8 @@ class ODEFuncGreedNonLin(ODEFuncGreed):
     elif self.opt['gnl_W_style'] == 'diag':
       if self.time_dep_unstruct_w:
         return torch.diag(self.gnl_W_D_T[T])
-      #todo chcek this
-      elif self.time_dep_unstruct_w or self.time_dep_struct_w:
-        if self.time_dep_unstruct_w:
-          W = self.gnl_W_D_T[T]
-        elif self.time_dep_struct_w:
-          W = self.gnl_W_D
+      elif self.time_dep_struct_w:
+        W = self.gnl_W_D
         alpha = torch.diag(torch.exp(self.brt[T] * T + self.brt[T]))
         beta = torch.diag(torch.exp(-self.brt[T] * T - self.crt[T]) + self.drt[T])
         Wplus = torch.diag(F.relu(W))
