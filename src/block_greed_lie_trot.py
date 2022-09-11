4993baf6fef13a47fe10b31bfd5daea451e13f3f
JRowbottomGit <jabrowbottom@gmail.com>
2022-09-11 14:55:22 +0100

    time

--- src/block_greed_lie_trot.py
+++ src/block_greed_lie_trot.py
@@ -25,8 +25,6 @@ class GREEDLTODEblock(ODEblock):
         self.unpack_blocks(opt)
     # needed for stats and TSNE plots
     self.cum_steps_list, self.cum_time_points, self.cum_time_ticks, self.block_type_list = create_time_lists(self.opt) #self.create_time_lists()
-    print("times")
-    print(create_time_lists(self.opt))
     for block_num, lt2_args in enumerate(self.opt['lt_gen2_args']):
       opt2 = self.opt.as_dict().copy()
       # print(f"lt_block_type {opt2['lt_block_type']}")
@@ -69,6 +67,9 @@ class GREEDLTODEblock(ODEblock):
     self.times = times
     self.steps = steps
 
+    print("times")
+    print(create_time_lists(self.opt))
+    print(self.times)
     #adding the first func in module list as block attribute to match signature required in run_GNN.py
     # ie model.odeblock.odefunc.epoch
     self.odefunc = self.funcs[0]
